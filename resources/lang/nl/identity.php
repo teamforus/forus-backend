da1d84e4b973cdba9b7a2cc5dc8c18156182c2bd
GerbenB <gerben@forus.io>
2018-10-05 13:43:36 +0200

    Added dutch translation to all the error messages

---
+++ resources/lang/nl/identity.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    "exceptions" => [
+        "unknown_identity" => "Deze gegevens zijn niet bekend",
+        "invalid_pin_code" => "Ongeldige invoer van de oude code",
+    ]
+];
