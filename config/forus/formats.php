2e2da116c234c4ff7536a82c9528a04fa9295d2f
Max Visser <maxvisser@gmail.com>
2018-10-28 13:34:47 +0100

    Update formats.php

--- config/forus/formats.php
+++ config/forus/formats.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'short_date_time' => 'H:i d M, Y',
-    'short_date_time_locale' => '%H:%m %d %b, %Y',
+    'short_date_time' => 'H:i - d M, Y',
+    'short_date_time_locale' => '%H:%m - %d %b, %Y',
 ];
