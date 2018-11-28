1d939c926a7d458c564e9426bcb605a04a36fb38
Yarik <ateamcms@gmail.com>
2018-11-28 12:53:35 +0200

    Fix #291 pagination product on home webshop

--- app/Http/Controllers/Api/Platform/ProductsController.php
+++ app/Http/Controllers/Api/Platform/ProductsController.php
@@ -8,22 +8,40 @@ use App\Models\FundProvider;
 use App\Models\Implementation;
 use App\Models\Product;
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 
 class ProductsController extends Controller
 {
     /**
      * Display a listing of the resource
      *
+     * @param Request $request
      * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection
-     * @throws \Illuminate\Auth\Access\AuthorizationException
      */
-    public function index()
+    public function index(Request $request)
     {
         $this->authorize('indexPublic', Product::class);
 
         $organizationIds = FundProvider::query()->whereIn(
             'fund_id', Implementation::activeFunds()->pluck('id')
         )->where('state', 'approved')->pluck('organization_id');
+
+        if($request->has('forHome')) {
+            $total = $request->input('total', 6);
+
+            return ProductResource::collection(Product::query()
+                ->whereIn(
+                    'organization_id', $organizationIds
+                )
+                ->where('sold_out', false)
+                ->where(
+                    'expire_at', '>', date('Y-m-d')
+                )
+                ->groupBy('organization_id')
+                ->inRandomOrder()
+                ->take($total)
+                ->get());
+        }
 
         return ProductResource::collection(Product::query()->whereIn(
             'organization_id', $organizationIds
