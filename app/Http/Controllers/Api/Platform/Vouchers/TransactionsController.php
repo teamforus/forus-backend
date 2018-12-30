aee26f55aa3aebe75fda6b153b7a8cd29add8e87
dev-rminds <dev@rminds.nl>
2018-12-30 03:22:40 +0200

    Don't send payment_success for product vouchers #548

--- app/Http/Controllers/Api/Platform/Vouchers/TransactionsController.php
+++ app/Http/Controllers/Api/Platform/Vouchers/TransactionsController.php
@@ -99,7 +99,9 @@ class TransactionsController extends Controller
             $product->updateSoldOutState();
         }
 
-        $voucher->sendEmailAvailableAmount();
+        if (!$product) {
+            $voucher->sendEmailAvailableAmount();
+        }
 
         $note = $request->input('note', false);
 
