415093da134121ec5574926640c561e69a56d13a
Max Visser <max@forus.io>
2019-01-25 14:41:06 +0100

    Update push.php

--- resources/lang/nl/push.php
+++ resources/lang/nl/push.php
@@ -3,33 +3,33 @@
 return [
     "voucher" => [
         "activated" => [
-            "title" => "",
+            "title" => ":fund_name",
             "body" => "U hebt een activatiecode gebruikt om ':fund_name' voucher te activeren.",
         ],
         "bought" => [
-            "title" => "",
+            "title" => ":implementation_name",
             "body" => "U hebt een aanbieding gekocht in ':implementation_name' webshop.",
         ],
     ],
     "transactions" => [
         "offline_regular_voucher" => [
-            "title" => "",
+            "title" => ":fund_name",
             "body" => "Er is zojuist een betaling plaats gevonden van &euro;:amount van uw ':fund_name' voucher",
         ],
         "offline_product_voucher" => [
-            "title" => "",
+            "title" => ":product_name",
             "body" => "Een aanbieding voucher is zojuist gebruikt om :product_name af te nemen.",
         ]
     ],
     "bunq_transactions" => [
         "complete" => [
-            "title" => "",
+            "title" => "Uitbetaling",
             "body" => "Uw transactie van &euro;:amount is uitbetaald.",
         ]
     ],
     "access_levels" => [
         "added" => [
-            "title" => "",
+            "title" => ":org_name",
             "body" => "U bent toegevoegd aan :org_name as :role_name_list.",
         ],
         "updated" => [
@@ -37,13 +37,13 @@ return [
             "body" => "",
         ],
         "removed" => [
-            "title" => "",
+            "title" => ":org_name",
             "body" => "U bent verwijderd als een medewerker van :org_name.",
         ]
     ],
     "providers" => [
         "accepted" => [
-            "title" => "",
+            "title" => ":fund_name",
             "body" => "U bent goedgekeurd voor :fund_name.",
         ]
     ]
