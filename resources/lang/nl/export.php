eef3cfa454f5d65fcc42414137db3d44a8ef7f0d
GerbenB <gerben@forus.io>
2019-01-29 14:34:01 +0100

    added text for export function fixes teamforus/forus#736

--- resources/lang/nl/export.php
+++ resources/lang/nl/export.php
@@ -1,3 +1,24 @@
 <?php
 
-return [];
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Export Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines contain the default headers for exporting the list of providers used by
+    | the sponsor class. Some of these rules have multiple versions such
+    | as the size rules. Feel free to tweak each of these messages here.
+    |
+    */
+    
+    'providers'              => [
+		'provider' => 'Aanbieder',
+		'email' => 'e-mail',
+		'phone' => 'telefoonnummer',
+        'categories' => 'categoriën',
+        'kvk' => 'kvk',
+        'state' => 'status',
+    ]
+];
