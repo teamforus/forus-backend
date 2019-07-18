af452952bb58d0fcfc151ba02da3f3f012d9c4a5
dev-rminds <dev@rminds.nl>
2019-07-18 19:39:38 +0300

    remove unused code

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,10 +4,6 @@ namespace App\Exceptions;
 
 use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
-use Illuminate\Http\Exceptions\HttpResponseException;
-use Illuminate\Http\JsonResponse;
-use Illuminate\Support\Arr;
-use Illuminate\Support\Collection;
 
 class Handler extends ExceptionHandler
 {
@@ -54,34 +50,5 @@ class Handler extends ExceptionHandler
     public function render($request, Exception $exception)
     {
         return parent::render($request, $exception);
-    }
-
-    /**
-     * Convert the given exception to an array.
-     *
-     * @param  \Exception  $e
-     * @return array
-     */
-    protected function convertExceptionToArray(Exception $e)
-    {
-        if ($e->getMessage() instanceof Collection) {
-            $message = collect($e->getMessage())->toArray();
-
-            $data = config('app.debug') ? $message :
-                $this->isHttpException($e) ? $message : [
-                'message' => 'Server Error'
-            ];
-
-            return array_merge(config('app.debug') ? [
-                'exception' => get_class($e),
-                'file' => $e->getFile(),
-                'line' => $e->getLine(),
-                'trace' => collect($e->getTrace())->map(function ($trace) {
-                    return Arr::except($trace, ['args']);
-                })->all(),
-            ] : [], $data);
-        }
-
-        return parent::convertExceptionToArray($e);
     }
 }
