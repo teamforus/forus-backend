0ab9a86e022451b2ac8f4337f804f1de02b0967c
Max Visser <maxvisser@gmail.com>
2019-08-01 15:07:18 +0200

    added New Validation Request

---
+++ resources/views/emails/validations/new_validation_request.blade.php
@@ -0,0 +1,12 @@
+@extends('emails.base)
+
+@section('title', trans('mails.validations.you_added_as_validator.title'))
+@section('html')
+    Beste validator,
+    <br/>
+    <br/>
+    Er staat een verzoek voor u klaar om eigenschappen te valideren.
+    <br/>
+    <br/>
+    Ga naar het validator dashboard <a href="{{ $validator_dashboard_link }}" target="_blank" style="color: #315efd; text-decoration: underline;">{{ $validator_dashboard_link }}</a> om dit verzoek te behandelen.
+@endsection
---
+++ resources/views/emails/validations/you_added_as_validator.blade.php
@@ -0,0 +1,12 @@
+@extends('emails.base)
+
+@section('title', trans('mails.validations.you_added_as_validator.title'))
+@section('html')
+    Beste medewerker,
+    <br/>
+    <br/>
+    {{ $sponsor_name }} heeft u toegevoegd als validator.
+    <br/>
+    Vanaf nu kunt u aanvragers toevoegen, dit kunt u doen door naar het dashboard te gaan een een .CSV bestand te uploaden.
+    <br/>
+@endsection
