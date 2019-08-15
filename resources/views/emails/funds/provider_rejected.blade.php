cf138c95609eaa9906e5be83a9972d20aaf38a87
eranmachiels <dev@eranmachiels.nl>
2019-08-15 16:55:24 +0200

    Fixed missing variables

--- resources/views/emails/funds/provider_rejected.blade.php
+++ resources/views/emails/funds/provider_rejected.blade.php
@@ -1,8 +1,8 @@
 @extends('emails.base')
 
-@section('title', implementation_trans('provider_rejected.title'))
+@section('title', implementation_trans('provider_rejected.title', ['provider_rejected' => ]))
 @section('html')
-    {{ implementation_trans('dear_provider') }},
+    {{ implementation_trans('dear_provider', ['provider_name' => $provider_name]) }},
     <br/>
     <br/>
     {{ implementation_trans('provider_rejected.applied_for_fund', ['fund_name' => $fund_name]) }}
@@ -15,7 +15,7 @@
     <br/>
     {{ implementation_trans('provider_rejected.want_to_know_more', ['sponsor_name' => $sponsor_name]) }}
     <br />
-    {{ implementation_trans('provider_rejected.phonenumber', ['phone_number' => '']) }}
+    {{ implementation_trans('provider_rejected.phone_number', ['phone_number' => '']) }}
     <br />
     {{ implementation_trans('hopefully_informed_enough') }}
     <br/>
@@ -23,6 +23,6 @@
     {{ implementation_trans('greets') }}
     <br/>
     <br/>
-    {{ implementation_trans('team_fund') }}
+    {{ implementation_trans('team_fund', ['fund_name' => $fund_name]) }}
 @endsection
 
