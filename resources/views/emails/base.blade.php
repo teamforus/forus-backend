dc77f99318a42663b149e48f5b34ec326641064c
eranmachiels <dev@eranmachiels.nl>
2019-08-23 12:27:55 +0200

    Fixed implementation config bug

--- resources/views/emails/base.blade.php
+++ resources/views/emails/base.blade.php
@@ -4,14 +4,14 @@
     </head>
     <body style="{{ mail_config('base.body_style') }}" bgcolor="{{ mail_config('base.body_bg_color') }}">
     <center>
-        <table id="wrapperTable" cellpadding="0" cellspacing="0" border="0" style="{{ implementation_config('base.wrapper_table') }}" bgcolor="{{ implementation_config('base.body_bg_color') }}">
+        <table id="wrapperTable" cellpadding="0" cellspacing="0" border="0" style="{{ mail_config('base.wrapper_table') }}" bgcolor="{{ mail_config('base.body_bg_color') }}">
             <tr>
                 <td valign="top" align="center" style="border-collapse: collapse;">
-                    <div id="wrapper" style="{{ implementation_config('base.wrapper') }}">
-                        <table cellpadding="0" cellspacing="0" border="0" style="{{ implementation_config('base.container') }}" bgcolor="{{ implementation_config('base.body_bg_color') }}">
+                    <div id="wrapper" style="{{ mail_config('base.wrapper') }}">
+                        <table cellpadding="0" cellspacing="0" border="0" style="{{ mail_config('base.container') }}" bgcolor="{{ mail_config('base.body_bg_color') }}">
                             <tr>
                                 <td style="border-collapse: collapse; padding: 24px 24px 32px;">
-                                    <h1 style="{{ implementation_config('base.h1_style') }}"> @yield('title')</h1>
+                                    <h1 style="{{ mail_config('base.h1_style') }}"> @yield('title')</h1>
                                 </td>
                             </tr>
                             <tr>
@@ -36,8 +36,8 @@
                                 <td align="center" style="border-collapse: collapse;">
                                     <table cellpadding="0" cellspacing="0" border="0" style="border-collapse: collapse; mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-size: 0; width: 166px!important; text-align: center; margin: 0px auto;">
                                         <tr>
-                                            <td align="center" style="{{ implementation_config('base.button_td') }}">
-                                                <a href="@yield('link')" target="_blank" style="{{ implementation_config('base.button') }}">@yield('button_text')</a>
+                                            <td align="center" style="{{ mail_config('base.button_td') }}">
+                                                <a href="@yield('link')" target="_blank" style="{{ mail_config('base.button') }}">@yield('button_text')</a>
                                             </td>
                                         </tr>
                                     </table>
