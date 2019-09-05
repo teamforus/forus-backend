5c9045fa1d828829c7abc8ef37308352aa0f5ba0
eranmachiels <dev@eranmachiels.nl>
2019-09-05 12:38:28 +0200

    Added notification preferences relation

--- app/Services/Forus/MailNotification/Models/NotificationType.php
+++ app/Services/Forus/MailNotification/Models/NotificationType.php
@@ -3,8 +3,15 @@
 namespace App\Services\Forus\MailNotification\Models;
 
 use App\Models\Model;
+use App\Models\NotificationPreference;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class NotificationType extends Model
 {
     protected $fillable = ['notification_type'];
+
+    public function notificationPreferences(): HasMany
+    {
+        return $this->hasMany(NotificationPreference::class, 'notification_type_id', 'id');
+    }
 }
