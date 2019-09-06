2600786ad5aab3941240e8191bc703df1a599319
emachiels <e.machiels@hotmail.com>
2019-09-06 11:53:36 +0200

    Fixed balance warning emails not being sent

--- app/Models/Fund.php
+++ app/Models/Fund.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use App\Events\Vouchers\VoucherCreated;
 use App\Services\BunqService\BunqService;
+use App\Services\Forus\MailNotification\MailService;
 use App\Services\Forus\Record\Repositories\RecordRepo;
 use App\Services\MediaService\Models\Media;
 use App\Services\MediaService\Traits\HasMedia;
@@ -607,7 +608,10 @@ class Fund extends Model
      */
     public static function notifyAboutReachedNotificationAmount()
     {
+        /** @var MailService $mailService */
         $mailService = resolve('forus.services.mail_notification');
+        /** @var RecordRepo $recordRepo */
+        $recordRepo = resolve('forus.services.record');
 
         $funds = self::query()
             ->whereHas('fund_config', function (Builder $query){
@@ -628,17 +632,22 @@ class Fund extends Model
         foreach($funds as $fund) {
             if($fund->budget_left <= $fund->notification_amount) {
                 $referrers = $fund->organization->employeesOfRole('finance');
-                $referrers = $referrers->pluck('identity_address');
-                $referrers->push($fund->organization->emailServiceId());
+                $referrers = $referrers->pluck('identity_address')->map(function ($identity) use ($recordRepo) {
+                    return [
+                        'identity' => $identity,
+                        'email' => $recordRepo->primaryEmailByAddress($identity),
+                    ];
+                });
+
+                $referrers->push([
+                    'identity' => $fund->organization->emailServiceId(),
+                    'email' => $fund->organization->email
+                ]);
 
                 foreach ($referrers as $referrer) {
-                    $email = (new RecordRepo)->primaryEmailByAddress(
-                        $referrer->identity_address
-                    );
-
                     $mailService->fundNotifyReachedNotificationAmount(
-                        $email,
-                        $referrer,
+                        $referrer['email'],
+                        $referrer['identity'],
                         config('forus.front_ends.panel-sponsor'),
                         $fund->organization->name,
                         $fund->name,
