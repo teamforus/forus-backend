4aaed656cf6a997f12250642646f4b2f6c7c9134
Max Visser <maxvisser@gmail.com>
2019-09-12 09:22:12 +0200

    Balance warning extra breaks

--- resources/views/emails/funds/balance_warning.blade.php
+++ resources/views/emails/funds/balance_warning.blade.php
@@ -5,11 +5,11 @@
     {{ mail_trans('dear_sponsor', ['sponsor_name' => $sponsor_name]) }}
     <br />
     <br />
-    {{ mail_trans('balance_warning.budget_reached', ['fund_name' => $fund_name, 'notification_amount' => $notification_amount]) }}
+    {{ mail_trans('balance_warning.budget_reached', ['fund_name' => $fund_name, 'notification_amount' => $notification_amount]) }} <br />
     <br />
-    {{ mail_trans('balance_warning.budget_left_fund', ['fund_name' => $fund_name, 'budget_left' => $budget_left]) }}
+    {{ mail_trans('balance_warning.budget_left_fund', ['fund_name' => $fund_name, 'budget_left' => $budget_left]) }} <br />
     <br />
-    {{ mail_trans('balance_warning.no_transactions' }}
+    {{ mail_trans('balance_warning.no_transactions') }} <br />
     <br/>
     {{ mail_trans('balance_warning.you_can_login') }}
 @endsection
