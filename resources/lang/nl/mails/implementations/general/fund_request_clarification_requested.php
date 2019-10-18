e647cb0982e587063b03d9e1faba3778ea26572f
Max Visser <maxvisser@gmail.com>
2019-10-18 14:22:23 +0200

    added clarification mail text as a test

---
+++ resources/lang/nl/mails/implementations/general/fund_request_clarification_requested.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'title' => 'Aanvraag :fund_name aanvullen.',
+    'message' => 'Uw aanvraag voor :fund_name is onvolledig. De gemeente heeft meer informatie nodig om uw aanvraag af te handelen. <br /> <br /> Het bericht is: <b> :question </b>',
+    'webshop_button' => '<a href=":link" target="_blank" style="color: #315efd; text-decoration: underline;">:link</a>'
+];
