0899f075f686bf2539660bc68a555d7870006d1c
aghimpu <aghimpu90@gmail.com>
2019-10-30 15:25:33 +0200

    Fund criteria, add new fields

---
+++ database/migrations/2019_10_30_101955_add_fund_criteria_settings.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddFundCriteriaSettings extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('fund_criteria', function(Blueprint $table) {
+            $table->boolean('show_attachment')->default(true)
+                ->after('operator');
+            $table->text('description')
+                ->after('show_attachment');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('fund_criteria', function(Blueprint $table) {
+            $table->dropColumn('show_attachment');
+            $table->dropColumn('description');
+        });
+    }
+}
