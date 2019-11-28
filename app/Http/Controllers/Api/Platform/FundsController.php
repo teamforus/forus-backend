9b2fc6dab161d5986c68caea6a88faf604a3b9c2
aghimpu <aghimpu90@gmail.com>
2019-11-28 12:32:41 +0200

    Fix funds call issue

--- app/Http/Controllers/Api/Platform/FundsController.php
+++ app/Http/Controllers/Api/Platform/FundsController.php
@@ -26,7 +26,7 @@ class FundsController extends Controller
         return FundResource::collection(Fund::search(
             $request,
             Implementation::activeFundsQuery()
-        ));
+        )->get());
     }
 
     /**
