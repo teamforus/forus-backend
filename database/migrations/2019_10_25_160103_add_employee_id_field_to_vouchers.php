007a442f6ac6634c089a0e24c3734e6beb4428e6
Ghimpu Alexandru <aghimpu90@gmail.com>
2020-01-02 20:50:25 +0200

    Update 2019_10_25_160103_add_employee_id_field_to_vouchers.php

--- database/migrations/2019_10_25_160103_add_employee_id_field_to_vouchers.php
+++ database/migrations/2019_10_25_160103_add_employee_id_field_to_vouchers.php
@@ -20,20 +20,6 @@ class AddEmployeeIdFieldToVouchers extends Migration
             $table->foreign('employee_id'
             )->references('id')->on('employees')->onDelete('set null');
         });
-
-        Voucher::whereNull('parent_id')->where(function(Builder $query) {
-            $vouchers = $query->whereNotNull('note')
-                ->orWhereNull('identity_address')->get();
-
-            $vouchers->each(function(Voucher $voucher) {
-                $employees = $voucher->fund->organization->employees();
-                $voucher->update([
-                    'employee_id' => $employees->whereHas('roles', function(Builder $query) {
-                        return $query->where('key', 'validation');
-                    })->first()->id ?? null
-                ]);
-            });
-        });
     }
 
     /**
