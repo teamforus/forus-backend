0701ca0f366a1df14ca6ac889d9d6151f247b3e3
aghimpu <aghimpu90@gmail.com>
2020-01-16 20:35:47 +0200

    Allow duplicate kvk numbers for organizations on debug mode

--- app/Http/Requests/Api/Platform/Organizations/StoreOrganizationRequest.php
+++ app/Http/Requests/Api/Platform/Organizations/StoreOrganizationRequest.php
@@ -36,7 +36,7 @@ class StoreOrganizationRequest extends FormRequest
             'kvk'                   => [
                 'required',
                 'digits:8',
-                'unique:organizations,kvk',
+                !config('app.debug') ? 'unique:organizations,kvk' : null,
                 new KvkRule()
             ],
             'btw'                   => ['nullable', 'string', new BtwRule()],
