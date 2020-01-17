6d4c3380323b365533ffdb88bbd46a6eee169fc1
aghimpu <aghimpu90@gmail.com>
2020-01-17 12:43:39 +0200

    Fund closed email to provider, updated some texts

--- resources/views/emails/funds/fund_closed_provider.blade.php
+++ resources/views/emails/funds/fund_closed_provider.blade.php
@@ -5,7 +5,12 @@
     {{ mail_trans('dear_provider', ['provider_name' => $provider_name]) }}
     <br />
     <br />
-    {{ mail_trans('fund_closed_provider.description', ['fund_name' => $fund_name, 'end_date' => $fund_end_date]) }} <br />
+    {{ mail_trans('fund_closed_provider.description', [
+            'fund_name' => $fund_name,
+            'end_date'  => $fund_end_date,
+            'end_date_minus1' => $fund_end_date->subDay()
+        ])
+    }} <br />
     <br/>
     {!! mail_trans('fund_closed_provider.dashboard_link', ['link' => $dashboard_link]) !!}
 @endsection
