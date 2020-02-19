5758fd886f8f6ada5a5b6f9a49246b4035d8de64
GerbenB <gerben@forus.io>
2020-02-19 12:51:42 +0100

    textual changes for voucher_send

--- resources/views/emails/vouchers/voucher_sent.blade.php
+++ resources/views/emails/vouchers/voucher_sent.blade.php
@@ -7,10 +7,19 @@
     <br/>
     {{ mail_trans('voucher_sent.you_have_asked', ['product_name' => $fund_product_name]) }}
     <br/>
-    {{ mail_trans('voucher_sent.qr_code_under') }}
+    <br/>
+    {{ mail_trans('voucher_sent.qr_code_under',[
+        'voucher_amount'      => $voucher_amount,
+        'expire_at_minus_day' => $voucher_expire_minus_day 
+    ]) }}
+    <br/>
+    <img style="display: block; margin: 0 auto;" alt="" src="{{ $message->embedData(make_qr_code('voucher', $qr_token), 'qr_token.png') }}" width="300" />
     <br/>
     {{ mail_trans('voucher_sent.provider_scans') }}
     <br/>
     <br/>
-    <img style="display: block; margin: 0 auto;" alt="" src="{{ $message->embedData(make_qr_code('voucher', $qr_token), 'qr_token.png') }}" width="300" />
+    {{ mail_trans('voucher_sent.purchase_notice', ['voucher_amount' => $voucher_amount]) }}
+    <br/>
+    <br/>
+    {{ mail_trans('voucher_sent.have_fund') }}
 @endsection
