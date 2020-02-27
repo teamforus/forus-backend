f056c4be1e1aa5c177c9eb4fea83a4ebaaef44ff
Max Visser <max@forus.io>
2020-02-27 09:42:36 +0100

    Update product_reserved.php

--- resources/lang/nl/mails/implementations/general/product_reserved.php
+++ resources/lang/nl/mails/implementations/general/product_reserved.php
@@ -5,6 +5,6 @@ return [
     'product_reserved' => 'Deze mail ontvangt u na aanleiding van dat er een aanbieding bij :provider_organization_name is gereserveerd.',
     'deadline'   => 'De reservering heeft een waarde van €:price en is geldig tot en met :expire_at_minus_1_day. '.
         'Wanneer de reservering is verlopen kunt u deze annuleren in de webshop.',
-    'contact_us' => 'Dit is de contactinformation van :provider_organization_name'.'<br/>'.
-        ':provider_phone'.'<br/>'.':provider_email',
+    'contact_us' => 'Dit is de contact informatie van :provider_organization_name'.'<br/>'.
+        'Telefoonnummer: :provider_phone'.'<br/>'.'Contact e-mailadres: :provider_email',
 ];
