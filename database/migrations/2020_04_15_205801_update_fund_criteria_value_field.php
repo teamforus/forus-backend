2635864dd591568a10dc0053ed405ec86bd4006e
aghimpu <aghimpu90@gmail.com>
2020-04-15 22:23:37 +0300

    Fund criteria, increase value character limit

---
+++ database/migrations/2020_04_15_205801_update_fund_criteria_value_field.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateFundCriteriaValueField extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('fund_criteria', function(Blueprint $table) {
+            $table->string('value', 20)->default('')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
