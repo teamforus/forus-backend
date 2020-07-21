0796647754e0a5971df62364e7c2aaf9d021dd23
Max Visser <max@forus.io>
2020-07-21 11:39:31 +0200

    Added feedback from figma; Requester digest

--- resources/lang/nl/digests/requester.php
+++ resources/lang/nl/digests/requester.php
@@ -5,21 +5,21 @@ return [
     'providers' => [
         'title' => implode('|', [
             // singular
-            ":sponsor_name zijn goedgekeurd :count_providers nieuwe aanbieder voor :fund_name",
+            ":sponsor_name heeft :count_providers nieuwe aanbieder toegevoegd aan :fund_name",
             // plural
-            ":sponsor_name zijn goedgekeurd :count_providers nieuwe aanbieders voor :fund_name"
+            ":sponsor_name heeft :count_providers nieuwe aanbieders toegevoegd aan :fund_name"
         ]),
         'description' =>
-            "Uw kunt uw tegoed bij de volgende nieuwe aanbieders: :providers_list\n\n" .
-            "Bekijk de webshop voor de volledige context en status.",
+            "Uw tegoed kunt u nu uitgeven bij: :providers_list\n\n" .
+            "Kijk op de webshop voor meer informatie over de aanbieders.",
     ],
 
     'products' => [
         'title' => implode('|', [
             // singular
-            ":sponsor_name zijn geaccepteerd :count_products nieuwe product voor :fund_name",
+            ":sponsor_name heeft :count_products nieuwe aanbieding aan de webshop toegevoegd van :fund_name.",
             // plural
-            ":sponsor_name zijn geaccepteerd :count_products nieuwe producten voor :fund_name"
+            ":sponsor_name heeft :count_products nieuwe aanbiedingen aan de webshop toegevoegd van :fund_name."
         ]),
         'price' => "- :product_name voor €:product_price_locale",
     ],
