7a28845508c10b14ec58d6755a47ee0c525aff1e
Max Visser <max@forus.io>
2020-07-21 11:53:47 +0200

    Figma Feedback: provider products digest

--- resources/lang/nl/digests/provider_products.php
+++ resources/lang/nl/digests/provider_products.php
@@ -2,19 +2,19 @@
 
 return [
     'title' => implode('|', [
-        'Overzicht: :count_products nieuwe product gereserveerd :provider_name',
-        'Overzicht: :count_products nieuwe producten gereserveerd :provider_name',
+        'Overzicht: :count_products nieuwe aanbieding reservering',
+        'Overzicht: :count_products nieuwe aanbieding reserveringen',
     ]),
     'greetings' => implode('|', [
-        "Beste :provider_name,\nEr zijn :count_products product gereserveerd vandaag.",
-        "Beste :provider_name,\nEr zijn :count_products producten gereserveerd vandaag.",
+        "Beste :provider_name,\nVandaag is er :count_products aanbieding gereserveerd.",
+        "Beste :provider_name,\nVandaag zijn er :count_products aanbiedingen gereserveerd.",
     ]),
-    'fund_title' => 'Uw product is gereserveerd met :fund_name',
+    'fund_title' => 'Uw aanbieding is gereserveerd met :fund_name',
     'fund_products' => implode('|', [
-        "- :product_name - :count_reservations reservering(en)\n" .
-        "De laatste dag dat de klant uw product kan ophalen is :fund_end_date_locale",
-        "- :product_name - :count_reservations reservering(en)\n" .
-        "De laatste dag dat de klant uw product kan ophalen is :fund_end_date_locale",
+        "- :product_name - :count_reservations reservering\n" .
+        "De klant dient de reservering voor :fund_end_date_locale te gebruiken.",
+        "- :product_name - :count_reservations reserveringen\n" .
+        "De klant dient de reservering voor :fund_end_date_locale te gebruiken.",
     ]),
     'dashboard_button' => 'GA NAAR HET DASHBOARD',
 ];
