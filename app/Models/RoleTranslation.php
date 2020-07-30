293014ded93fffa85db01a067faa6ebd7a8a85c8
dev-rminds <dev@rminds.nl>
2020-07-30 10:03:52 +0300

    fix merge conflict

--- app/Models/RoleTranslation.php
+++ app/Models/RoleTranslation.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
- * Class RoleTranslation
+/** Class RoleTranslation
  *
  * @package App\Models
  * @property int $id
