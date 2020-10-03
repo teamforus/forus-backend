a6f20c91c447036eb29686155abeef0a871d7128
aghimpu <aghimpu90@gmail.com>
2020-10-03 23:59:59 +0300

    Fund request clarification mail adjustments

--- resources/lang/nl/mails/implementations/general/fund_request_clarification_requested.php
+++ resources/lang/nl/mails/implementations/general/fund_request_clarification_requested.php
@@ -1,8 +1,6 @@
 <?php
 
 return [
-    'title' => 'Aanvraag :fund_name aanvullen.',
-    'message' => 'Uw aanvraag voor :fund_name is onvolledig. De gemeente heeft meer informatie nodig om uw aanvraag af te handelen.',
-    'question' => 'Het bericht is: :question',
-    'webshop_button' => '<a href=":link" target="_blank" style="color: #315efd; text-decoration: underline;">:link</a>'
+    'title'       => 'Uw aanvraag voor :fund_name aanvullen.',
+    'button_text' => 'Ga naar uw aanvraag'
 ];
