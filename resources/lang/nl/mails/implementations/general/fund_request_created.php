1202981fdcc642688cc0a4b1b819b126ca853c0f
Max Visser <max@forus.io>
2020-10-05 08:28:07 +0200

    Update fund_request_created.php

--- resources/lang/nl/mails/implementations/general/fund_request_created.php
+++ resources/lang/nl/mails/implementations/general/fund_request_created.php
@@ -2,6 +2,6 @@
 
 return [
     'title' => 'Uw :fund_name aanvraag is ontvangen.',
-    'message' => ':sponsor_name note heeft uw :fund_name aanvraag ontvangen. Uw aanvraag is in behandeling. U ontvangt binnen twee weken een reactie op uw aanvraag.',
+    'message' => ':sponsor_name heeft uw :fund_name aanvraag ontvangen. Uw aanvraag is in behandeling. U ontvangt binnen twee weken een reactie op uw aanvraag.',
     'webshop_button' => 'Ga naar de webshop'
 ];
