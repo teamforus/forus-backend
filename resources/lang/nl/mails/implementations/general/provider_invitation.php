647bca51b656df89dd4fc9d7504e388bc50bb655
GerbenB <gerben@forus.io>
2020-10-15 14:12:56 +0200

    smal adjustments to invitation mail

--- resources/lang/nl/mails/implementations/general/provider_invitation.php
+++ resources/lang/nl/mails/implementations/general/provider_invitation.php
@@ -5,10 +5,10 @@ return [
     'dear_provider' => 'Beste ":provider_name",',
 
     'invitation_text' => join("\n\n", [
-        "U krijgt deze e-mail omdat u aangemeld staat voor \":from_fund_name\". Vanaf :fund_start_date zal \":sponsor_name\" een nieuw fonds uitgeven: \":fund_name\".",
+        "U krijgt deze e-mail omdat uw organisatie aangemeld is voor \":from_fund_name\". Vanaf :fund_start_date gaat \":sponsor_name\" het nieuwe \":fund_name\". uitgeven",
         "De periode loopt van :fund_start_date tot :fund_end_date. Ook tijdens deze periode kunt u een aanbod (opnieuw) plaatsen.",
         "We hopen dat u meedoet.",
-        "Heeft u vragen of loopt u tegen problemen aan bij de aanmelding? Dan kunt u een mail sturen naar :sponsor_email of bel naar telefoonnummer :sponsor_phone.",
+        "Heeft u vragen of loopt u tegen problemen aan bij de aanmelding? Dan kunt u een e-mail sturen naar :sponsor_email of bel naar telefoonnummer :sponsor_phone.",
     ]),
 
     'button_text' => 'AANMELDEN',
