462126f61dd515e2f15a0c581afeeef28e91aad9
aghimpu <aghimpu90@gmail.com>
2020-11-22 22:55:01 +0200

    Add back e-mail about product reservation which is sent to provider

--- app/Notifications/Identities/Voucher/IdentityProductVoucherReservedNotification.php
+++ app/Notifications/Identities/Voucher/IdentityProductVoucherReservedNotification.php
@@ -2,6 +2,7 @@
 
 namespace App\Notifications\Identities\Voucher;
 
+use App\Mail\Vouchers\ProductReservedMail;
 use App\Mail\Vouchers\ProductReservedUserMail;
 use App\Models\Implementation;
 use App\Models\Voucher;
@@ -28,6 +29,15 @@ class IdentityProductVoucherReservedNotification extends BaseIdentityVoucherNoti
                 'qr_token'  => $voucher->token_without_confirmation->address,
                 'expire_at_locale' => $this->eventLog->data['voucher_expire_date_locale'],
             ]), Implementation::emailFrom($this->eventLog->data['implementation_key']))
+        );
+
+        notification_service()->sendMailNotification(
+            $identity->primary_email->email,
+            new ProductReservedMail(array_merge(
+                $this->eventLog->data, [
+                    'expiration_date' => $this->eventLog->data['voucher_expire_date_locale']
+                ]
+            ), Implementation::emailFrom())
         );
     }
 }
