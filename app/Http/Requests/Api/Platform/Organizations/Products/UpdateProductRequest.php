f3e03e9441b6e62ba319b3a43a50f1d163478b1e
aghimpu <aghimpu90@gmail.com>
2020-12-06 05:44:33 +0200

    Update product request, no_price_type rule adjustment

--- app/Http/Requests/Api/Platform/Organizations/Products/UpdateProductRequest.php
+++ app/Http/Requests/Api/Platform/Organizations/Products/UpdateProductRequest.php
@@ -39,7 +39,7 @@ class UpdateProductRequest extends FormRequest
             'name'                  => 'required|between:2,200',
             'description'           => 'required|between:5,1000',
             'no_price'              => 'boolean',
-            'no_price_type'         => 'required_with:no_price|in:free,discount',
+            'no_price_type'         => 'required_if:no_price,true|in:free,discount',
             'no_price_discount'     => 'nullable|required_if:no_price_type,discount|numeric|min:0|max:100',
             'price'                 => $product->no_price ? [] : 'required_without:no_price|numeric|min:.2',
             'old_price'             => $product->no_price ? [] : [
