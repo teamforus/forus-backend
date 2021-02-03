6e38146af94f20fd30eee76af3cd6227cbf539d5
aghimpu <aghimpu90@gmail.com>
2021-02-03 07:56:19 +0200

    CMS privacy page description code refactoring

--- resources/lang/nl/record.php
+++ resources/lang/nl/record.php
@@ -3,5 +3,6 @@
 return [
     "exceptions" => [
         "unknown_record_type" => 'Onbekende type eigenschap ":type".',
+        "cant_delete_primary_email" => 'Uw ":record_type_name" kunt u niet verwijderen.'
     ]
 ];
