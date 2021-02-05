bcdce2ea3975f32433593a296de04e83d50c5d1b
dev-rminds <dev@rminds.nl>
2021-02-05 06:48:03 +0200

    refactoring office requests

--- app/Http/Requests/Api/Platform/Organizations/Offices/BaseOfficeRequest.php
+++ app/Http/Requests/Api/Platform/Organizations/Offices/BaseOfficeRequest.php
@@ -2,10 +2,24 @@
 
 namespace App\Http\Requests\Api\Platform\Organizations\Offices;
 
-use Illuminate\Foundation\Http\FormRequest;
+use App\Http\Requests\BaseFormRequest;
 
-abstract class BaseOfficeRequest extends FormRequest
+/**
+ * Class BaseOfficeRequest
+ * @package App\Http\Requests\Api\Platform\Organizations\Offices
+ */
+abstract class BaseOfficeRequest extends BaseFormRequest
 {
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize(): bool
+    {
+        return $this->isAuthenticated();
+    }
+
     /**
      * @return array|mixed
      */
@@ -31,21 +45,49 @@ abstract class BaseOfficeRequest extends FormRequest
             }
 
             $week_day = $scheduleMap[$index];
+            $dateLocale = $date->startOfWeek()->addDays($week_day)->formatLocalized('%A');
 
-            $attributes["schedule.$week_day.start_time"] =
-                $date->startOfWeek()->addDays($week_day)
-                    ->formatLocalized('%A') . ' start time';
-            $attributes["schedule.$week_day.end_time"] =
-                $date->startOfWeek()->addDays($week_day)
-                    ->formatLocalized('%A') . ' end time';
-            $attributes["schedule.$week_day.break_start_time"] =
-                $date->startOfWeek()->addDays($week_day)
-                    ->formatLocalized('%A') . ' break start time';
-            $attributes["schedule.$week_day.break_end_time"] =
-                $date->startOfWeek()->addDays($week_day)
-                    ->formatLocalized('%A') . ' break end time';
+            $attributes["schedule.$week_day.start_time"] = $dateLocale . ' start time';
+            $attributes["schedule.$week_day.end_time"] = $dateLocale . ' end time';
+
+            $attributes["schedule.$week_day.break_start_time"] = $dateLocale . ' break start time';
+            $attributes["schedule.$week_day.break_end_time"] = $dateLocale . ' break end time';
         }
 
         return $attributes;
+    }
+
+    /**
+     * @return array
+     */
+    protected function updateRules(): array
+    {
+        return [
+            'name' => '',
+            'phone' => '',
+            'email' => 'nullable|email:strict,dns',
+            'address' => 'required',
+            'schedule' => 'present|array',
+            'schedule.*' => 'required|array',
+            'schedule.*.week_day' => 'required|numeric:between:0,6',
+            'schedule.*.start_time' => [
+                'required_with:schedule.*.end_time',
+                'date_format:H:i'
+            ],
+            'schedule.*.end_time' => [
+                'required_with:schedule.*.start_time',
+                'date_format:H:i',
+                'after:schedule.*.start_time'
+            ],
+            'schedule.*.break_start_time' => [
+                'required_with:schedule.*.break_end_time',
+                'date_format:H:i'
+            ],
+            'schedule.*.break_end_time' => [
+                'required_with:schedule.*.break_start_time',
+                'date_format:H:i',
+                'after:schedule.*.break_start_time'
+            ],
+        ];
     }
 }
