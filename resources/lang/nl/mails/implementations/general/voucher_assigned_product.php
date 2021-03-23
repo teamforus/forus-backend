de13db9a15be3dc7e24731f0492e2f40e36dd784
Max Visser <maxvisser@gmail.com>
2021-03-23 17:21:30 +0100

    added notification text informal

--- resources/lang/nl/mails/implementations/general/voucher_assigned_product.php
+++ resources/lang/nl/mails/implementations/general/voucher_assigned_product.php
@@ -2,7 +2,7 @@
 
 return [
     'title_formal' => 'Hierbij ontvangt u uw :product_name',
-    'title_informal' => 'Hierbij ontvangt u uw :product_name',
+    'title_informal' => 'Hierbij ontvang je jouw :product_name',
     'paragraph1' => 'Alstublieft, :sponsor_name heeft een :product_name van :provider_name aan u toegekend.',
     'paragraph2' => 'U kunt uw :product_name gebruiken tot en met :voucher_last_active_day.',
     'paragraph3' => 'Heeft u vragen? Stuur dan een e-mail naar :provider_email of bel naar :provider_phone.',
