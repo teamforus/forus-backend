cf6501bb4379ae02a714bdc726676bdf918ea8c2
dev-rminds <dev@rminds.nl>
2021-05-06 11:35:51 +0300

    fix voucher assigned email title

--- resources/views/emails/vouchers/voucher_assigned_budget.blade.php
+++ resources/views/emails/vouchers/voucher_assigned_budget.blade.php
@@ -1,6 +1,6 @@
 @extends('emails.base')
 
-@section('title', mail_trans('voucher_assigned_budget.title', $data))
+@section('title', mail_trans('voucher_assigned_budget.title_' . $communicationType, $data))
 @section('html')
     {{ mail_trans('dear_user') }}
     <br/>
