b020966850ccb6181c07bde715c1a6be81f539e9
dev-rminds <dev@rminds.nl>
2021-06-25 06:21:17 +0300

    fix prevalidation export filter

--- app/Models/Prevalidation.php
+++ app/Models/Prevalidation.php
@@ -185,14 +185,14 @@ class Prevalidation extends Model
 
         if ($request->has('q') && $q = $request->input('q')) {
             $prevalidations->where(static function(Builder $query) use ($q) {
-                $query->where('uid', 'like', "%{$q}%");
+                $query->where('uid', 'like', "%$q%");
                 $query->orWhereIn('id', static function(
                     \Illuminate\Database\Query\Builder $query
                 ) use ($q) {
                     $query->from(
                         (new PrevalidationRecord)->getTable()
                     )->where(
-                        'value', 'like', "%{$q}%"
+                        'value', 'like', "%$q%"
                     )->select('prevalidation_id');
                 });
             });
@@ -232,26 +232,25 @@ class Prevalidation extends Model
     public static function export(Request $request): Collection {
         $transKey = "export.prevalidations";
 
-        $query = self::search($request);
-        $query->update([
-            'exported' => true
+        $query = self::search($request)->with([
+            'prevalidation_records.record_type.translations',
         ]);
 
-        return $query->with([
-            'prevalidation_records.record_type.translations'
-        ])->get()->map(static function(Prevalidation $prevalidation) use ($transKey)  {
+        $data = $query->get()->map(static function(Prevalidation $prevalidation) use ($transKey)  {
             return collect([
                 trans("$transKey.code") => $prevalidation->uid,
                 trans("$transKey.used") => $prevalidation->state === self::STATE_USED ?
                     trans("$transKey.used_yes") : trans("$transKey.used_no"),
-            ])->merge($prevalidation->prevalidation_records->filter(static function(
-                PrevalidationRecord $record
-            ) {
+            ])->merge($prevalidation->prevalidation_records->filter(function(PrevalidationRecord $record) {
                 return strpos($record->record_type->key, '_eligible') === false;
-            })->pluck(
-                'value', 'record_type.name'
-            ))->toArray();
+            })->pluck('value', 'record_type.name'))->toArray();
         })->values();
+
+        (clone($query))->update([
+            'exported' => true
+        ]);
+
+        return $data;
     }
 
     /**
