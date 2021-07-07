f54eeecd7eaeb39a0f1115625d8a7e25a55a7b9b
Max Visser <max@forus.io>
2021-07-07 14:23:21 +0200

    Bulk upload translations v1

--- app/Rules/ProductReservations/ProviderProductReservationBatchItemStockRule.php
+++ app/Rules/ProductReservations/ProviderProductReservationBatchItemStockRule.php
@@ -89,12 +89,13 @@ class ProviderProductReservationBatchItemStockRule extends BaseRule
         }));
 
         // Sponsor total limit for the product reached.
+        // Total limit of %s for the product \"%s\" reached!
         if ($target_products_count > $product['limit_total_available']) {
-            return sprintf("Total limit of %s for the product \"%s\" reached!", $product['limit_total'], $product->name);
+            return sprintf("Het totale limiet van %s voor aanbod \"%s\" is bereikt!", $product['limit_total'], $product->name);
         }
-
+        // The total limit of %s for the voucher was reached!
         if ($target_voucher_products_count > $product['limit_available']) {
-            return sprintf('The total limit of %s for the voucher was reached!', $product['limit_per_identity']);
+            return sprintf('Het totale limiet van %s voor dit tegoed is bereikt!', $product['limit_per_identity']);
         }
 
         return true;
@@ -117,7 +118,7 @@ class ProviderProductReservationBatchItemStockRule extends BaseRule
 
         // Sponsor total limit for the product reached.
         if ($totalAmount > $voucher->amount_available) {
-            return "Not enough funds on the voucher.";
+            return "Onvoldoende tegoed.";
         }
 
         return true;
@@ -130,6 +131,6 @@ class ProviderProductReservationBatchItemStockRule extends BaseRule
      */
     public function message(): string
     {
-        return sprintf('Line: %s: %s', $this->index + 1, ($this->messageText ?: ''));
+        return sprintf('Lijn: %s: %s', $this->index + 1, ($this->messageText ?: ''));
     }
 }
