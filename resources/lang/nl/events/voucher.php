cfa7e550eca5da1d51df698762f27c0a776dbcb6
maxvisser <maxvisser@gmail.com>
2021-08-25 10:12:56 +0200

    Added more translations for events

--- resources/lang/nl/events/voucher.php
+++ resources/lang/nl/events/voucher.php
@@ -4,5 +4,12 @@ return [
     'created_budget' => 'Aangemaakt',
     'created_product' => 'Aangemaakt',
     'activated' => 'Geactiveerd',
+    'assigned' => 'Toegekend',
     'deactivated' => 'Gedeactiveerd',
+    'expired' => 'Verlopen',
+    'expired_budget' => 'Verlopen',
+    'expired_product' => 'Verlopen',
+    'transaction' => 'Transactie',
+    'transaction_subsidy' => 'Transactie',
+    'transaction_budget' => 'Transactie',
 ];
