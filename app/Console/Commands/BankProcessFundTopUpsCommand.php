7bb6fa065b4943eb31c28991f0d8b1599b074a6b
aghimpu <aghimpu90@gmail.com>
2022-03-17 18:18:19 +0200

    Fund topups command increase fetch interval

--- app/Console/Commands/BankProcessFundTopUpsCommand.php
+++ app/Console/Commands/BankProcessFundTopUpsCommand.php
@@ -32,7 +32,7 @@ class BankProcessFundTopUpsCommand extends Command
     /**
      * @var int Seconds to wait until next request to the API
      */
-    protected $fetchInterval = 1;
+    protected $fetchInterval = 5;
 
     /**
      * Execute the console command.
