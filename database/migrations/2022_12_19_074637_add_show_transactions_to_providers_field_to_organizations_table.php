7be50a943d45a527b68cf1e5848a9cf82822198b
aghimpu <aghimpu90@gmail.com>
2022-12-19 09:26:43 +0200

    add organization level flag for displaying transactions to providers

---
+++ database/migrations/2022_12_19_074637_add_show_transactions_to_providers_field_to_organizations_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::table('organizations', function (Blueprint $table) {
+            $table->boolean('show_transactions_to_providers')->default(false)->after('bank_cron_time');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::table('organizations', function (Blueprint $table) {
+            $table->dropColumn('show_transactions_to_providers');
+        });
+    }
+};
