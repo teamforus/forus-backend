a1975e946740a2a03de90208ddef2c4100eb8877
aghimpu <aghimpu90@gmail.com>
2023-01-04 13:52:15 +0200

    create provider notifications on product reservation cancel by client, add 'archive' tab for reservations on the webshop

---
+++ app/Events/ProductReservations/ProductReservationCanceledByClient.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Events\ProductReservations;
+
+class ProductReservationCanceledByClient
+{
+
+}
---
+++ app/Notifications/Organizations/Products/ProductReservationCanceledNotification.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Notifications\Organizations\Products;
+
+class ProductReservationCanceledNotification
+{
+
+}
