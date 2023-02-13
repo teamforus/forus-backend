58eee27256d0e9c48e717530aa9bbc7537007504
dev-rminds <dev@rminds.nl>
2023-02-13 15:59:39 +0200

    add config to ignore fund budget in voucher generator

---
+++ database/migrations/2023_02_13_144140_add_generator_ignore_fund_budget_to_fund_configs_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::table('fund_configs', function (Blueprint $table) {
+            $table->boolean('generator_ignore_fund_budget')->default(false)->after('limit_voucher_total_amount');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::table('fund_configs', function (Blueprint $table) {
+            $table->dropColumn('generator_ignore_fund_budget');
+        });
+    }
+};
