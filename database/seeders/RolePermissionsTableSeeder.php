38f3eac76a1652d9d5de3bccc24fb9392b74f709
Gerben <gerben@forus.io>
2023-02-23 10:29:16 +0100

    removed code that was commented

--- database/seeders/RolePermissionsTableSeeder.php
+++ database/seeders/RolePermissionsTableSeeder.php
@@ -12,47 +12,6 @@ class RolePermissionsTableSeeder extends Seeder
     /**
      * @var string[][]
      */
-    /**protected array $rolePermissions = [
-        "admin" => [
-            "manage_funds", "manage_providers", "manage_products", "manage_offices",
-            "view_finances", "validate_records", "scan_vouchers", "manage_provider_funds",
-            "manage_vouchers", "manage_employees", "manage_organization",
-            "manage_implementation", "manage_implementation_cms",
-            "manage_bank_connections", "manage_transaction_bulks",
-            "manage_reimbursements", "manage_implementation_notifications",
-            "view_funds", "view_person_bsn_data", "make_direct_payments", 'manage_fund_texts',
-        ],
-        "finance_manager" => [
-            "view_finances", "manage_vouchers", "manage_bank_connections",
-            'manage_reimbursements',
-        ],
-        "validation" => [
-            "validate_records", "view_funds", "view_person_bsn_data"
-        ],
-        "supervisor_validation" => [
-            "manage_validators", "view_funds",
-        ],
-        "policy_officer" => [
-            "view_funds", 'manage_providers', 'manage_products',
-            "manage_offices", "manage_provider_funds",
-        ],
-        "operation_officer" => [
-            "scan_vouchers",
-        ],
-        "implementation_manager" => [
-            "view_funds", "manage_implementation", "manage_implementation_cms",
-        ],
-        "implementation_cms_manager" => [
-            "view_funds", "manage_implementation_cms",
-        ],
-        "implementation_communication_manager" => [
-            "view_funds", "manage_implementation_cms", "manage_implementation_notifications"
-        ],
-        "finance_reader" => [
-            "view_finances",
-        ],
-    ];**/
-
     protected array $rolePermissions = [
         "admin" => [
             "manage_funds", "manage_providers", "manage_products", "manage_offices",
@@ -99,8 +58,6 @@ class RolePermissionsTableSeeder extends Seeder
             "view_finances",
         ],
     ];
-
-
 
     /**
      * Run the database seeds.
