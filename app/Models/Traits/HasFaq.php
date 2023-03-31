0cad9868099f05142afd115d4b952808b0bd2708
dev-rminds <dev@rminds.nl>
2023-03-31 08:01:39 +0300

    sort faq - refactoring and cleanup

--- app/Models/Traits/HasFaq.php
+++ app/Models/Traits/HasFaq.php
@@ -5,6 +5,7 @@ namespace App\Models\Traits;
 use App\Models\Faq;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
+use Throwable;
 
 /**
  * @mixin \Eloquent
@@ -22,6 +23,7 @@ trait HasFaq
     /**
      * @param array|null $faq
      * @return static
+     * @throws Throwable
      */
     public function syncFaqOptional(?array $faq = null): static
     {
@@ -31,16 +33,15 @@ trait HasFaq
     /**
      * @param array $faq
      * @return static
+     * @throws Throwable
      */
     public function syncFaq(array $faq): static
     {
         // remove faq not listed in the array
         $this->faq()->whereNotIn('id', array_filter(array_pluck($faq, 'id')))->delete();
 
-        foreach ($faq as $index => $question) {
-            $this->syncQuestion(array_merge($question, [
-                'order' => $index
-            ]));
+        foreach ($faq as $order => $question) {
+            $this->syncQuestion(array_merge($question, compact('order')));
         }
 
         return $this;
@@ -51,9 +52,11 @@ trait HasFaq
      *
      * @param array $question
      * @return HasFaq|Model
+     * @throws Throwable
      */
     protected function syncQuestion(array $question): Faq|Model
     {
+        /** @var Faq $faq */
         $faq = $this->faq()->find($question['id'] ?? null) ?: $this->faq()->create();
         $faq->updateModel(array_only($question, ['title', 'description', 'order']));
         $faq->syncDescriptionMarkdownMedia('cms_media');
