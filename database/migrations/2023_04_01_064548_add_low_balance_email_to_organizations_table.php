9ed83caf64d62914999cf09b6d8189ee4b4646e1
aghimpu <aghimpu90@gmail.com>
2023-04-01 08:22:17 +0300

    add low balance email for organizations

---
+++ database/migrations/2023_04_01_064548_add_low_balance_email_to_organizations_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::table('organizations', function (Blueprint $table) {
+            $table->string('low_balance_email', 200)->nullable()->after('website_public');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::table('organizations', function (Blueprint $table) {
+            $table->dropColumn('low_balance_email');
+        });
+    }
+};
