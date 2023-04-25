861b880e6516d1c9aaf85fa7e6a3ef8570b54e20
dev-rminds <dev@rminds.nl>
2023-04-25 20:49:05 +0300

    updated test data seeder, product fund limits test rafactoring

---
+++ config/forus/test_data/configs/default/organizations.php
@@ -0,0 +1,47 @@
+<?php
+
+return [
+    'Zuidhorn' => [
+        'offices_count' => 0,
+        'data' => [
+            'allow_custom_fund_notifications' => true,
+        ]
+    ],
+    'Nijmegen' => [
+        'offices_count' => 0,
+        'data' => [
+            'backoffice_available' => true,
+            'allow_budget_fund_limits' => true,
+            'allow_manual_bulk_processing' => true,
+            'allow_custom_fund_notifications' => true,
+        ]
+    ],
+    'Gemeente Groningen' => [
+        'offices_count' => 0,
+        'data' => [
+            'website_public' => true,
+            'business_type_id' => 585,
+            'is_sponsor' => true,
+            'is_provider' => true,
+            'is_validator' => true,
+            'validator_auto_accept_funds' => false,
+            'reservations_budget_enabled' => true,
+            'reservations_subsidy_enabled' => true,
+            'reservations_auto_accept' => false,
+            'reservation_phone' => "no",
+            'reservation_address' => "no",
+            'reservation_birth_date' => "no",
+            'manage_provider_products' => true,
+            'backoffice_available' => false,
+            'allow_batch_reservations' => true,
+            'allow_custom_fund_notifications' => true,
+            'allow_budget_fund_limits' => false,
+            'pre_approve_external_funds' => false,
+            'provider_throttling_value' => 100,
+            'fund_request_resolve_policy' => "apply_auto_available",
+            'bsn_enabled' => true,
+            'bank_cron_time' => "09:00:00",
+            'show_provider_transactions' => false,
+        ]
+    ],
+];
