77af89495ea71035b63a7bf7f614fa6241afb418
dev-rminds <dev@rminds.nl>
2023-05-08 08:38:17 +0300

    add log when contact form submitted but no contact email configured

--- app/Http/Controllers/ContactFormController.php
+++ app/Http/Controllers/ContactFormController.php
@@ -6,6 +6,7 @@ use App\Http\Requests\Api\Contact\SendContactFormRequest;
 use App\Mail\ContactForm\ContactFormMail;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Log;
 
 class ContactFormController extends Controller
 {
@@ -17,12 +18,16 @@ class ContactFormController extends Controller
      */
     public function send(SendContactFormRequest $request): JsonResponse
     {
-        $data = $request->validated();
+        $data = $request->only([
+            'name', 'email', 'phone', 'organization', 'message',
+        ]);
 
         if ($email = Config::get('forus.notification_mails.contact_form', false)) {
             resolve('forus.services.notification')->sendSystemMail($email, new ContactFormMail($data));
+        } else {
+            Log::error('Contact form submitted but no contact email is not set:', $data);
         }
 
-        return response()->json();
+        return new JsonResponse();
     }
 }
