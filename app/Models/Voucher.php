6868b4a4871456f28748724cb8d7efef7d3488d0
aghimpu <aghimpu90@gmail.com>
2023-06-09 13:40:12 +0300

    add amount available filter for vouchers

--- app/Models/Voucher.php
+++ app/Models/Voucher.php
@@ -711,6 +711,19 @@ class Voucher extends BaseModel
             $query->where('amount', '<=', $request->input('amount_max'));
         }
 
+        if ($request->has('amount_available_min') || $request->has('amount_available_max')) {
+            $query = VoucherQuery::addBalanceFields($query);
+            $query = Voucher::query()->fromSub($query, 'vouchers');
+        }
+
+        if ($request->has('amount_available_min')) {
+            $query->where('balance', '>=', $request->input('amount_available_min'));
+        }
+
+        if ($request->has('amount_available_max')) {
+            $query->where('balance', '<=', $request->input('amount_available_max'));
+        }
+
         if ($request->has('from')) {
             $query->where('created_at', '>=', Carbon::parse(
                 $request->input('from'))->startOfDay()
