c7b7c69d406372ebc84e5aeea2d1f4ba77341e53
dev-rminds <dev@rminds.nl>
2023-06-29 20:49:52 +0300

    add 2fa columns indexes

---
+++ database/migrations/2023_06_29_194612_add_2fa_indexes_to_organizations_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::table('organizations', function (Blueprint $table) {
+            $table->index([
+                'auth_2fa_policy', 'auth_2fa_remember_ip',
+            ]);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::table('organizations', function (Blueprint $table) {
+            $table->dropIndex('organizations_auth_2fa_policy_auth_2fa_remember_ip_index');
+        });
+    }
+};
