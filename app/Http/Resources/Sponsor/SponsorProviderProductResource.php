c9a124796fe5e485ea48dda5da7f144b42995c07
Yaroslav Kosterin <ateamcms@gmail.com>
2023-07-08 11:01:19 +0300

    add provider unseen messages to provider products

--- app/Http/Resources/Sponsor/SponsorProviderProductResource.php
+++ app/Http/Resources/Sponsor/SponsorProviderProductResource.php
@@ -65,21 +65,9 @@ class SponsorProviderProductResource extends BaseJsonResource
             'deleted' => !is_null($product->deleted_at),
             'photo' => new MediaResource($product->photo),
             'product_category' => new ProductCategoryResource($product->product_category),
-            'unseen_messages' => $this->hasUnseenMessages($product),
             'is_available' => $this->isAvailable($product, $fundProvider) ,
             'deals_history' => $fundProvider ? $this->getDealsHistory($product, $fundProvider) : null,
         ], $this->productReservationFieldSettings($product));
-    }
-
-    /**
-     * @param Product $product
-     * @return bool
-     */
-    protected function hasUnseenMessages(Product $product): bool
-    {
-        return FundProviderChatMessage::whereIn(
-            'fund_provider_chat_id', $product->fund_provider_chats()->pluck('id')
-        )->where('provider_seen', '=', false)->count();
     }
 
     /**
