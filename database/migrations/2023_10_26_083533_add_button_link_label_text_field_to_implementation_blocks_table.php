08e95f54650540f2367601adf2eb4725b17e0964
aghimpu <aghimpu90@gmail.com>
2023-10-26 12:29:29 +0300

    remove some unused code in the button link label migration

--- database/migrations/2023_10_26_083533_add_button_link_label_text_field_to_implementation_blocks_table.php
+++ database/migrations/2023_10_26_083533_add_button_link_label_text_field_to_implementation_blocks_table.php
@@ -3,7 +3,6 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-use App\Models\ImplementationBlock;
 
 return new class extends Migration
 {
