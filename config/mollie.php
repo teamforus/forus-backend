38d8cfa67a63c86165e9199f2a3d1c3ef4b898b8
dev-rminds <dev@rminds.nl>
2023-11-27 18:42:19 +0200

    fix default mollie webhook url

--- config/mollie.php
+++ config/mollie.php
@@ -5,7 +5,7 @@ return [
     'client_id' => env('MOLLIE_CLIENT_ID', ''),
     'client_secret' => env('MOLLIE_CLIENT_SECRET', ''),
     'redirect_url' => env('MOLLIE_REDIRECT_URI', '/mollie/callback'),
-    'webhook_url' => env('MOLLIE_WEBHOOK_URI', '/mollie/webhook'),
+    'webhook_url' => env('MOLLIE_WEBHOOK_URI', '/mollie/webhooks'),
 
     'base_access_token' => env('MOLLIE_ACCESS_TOKEN', ''),
     'token_expire_offset' => env('MOLLIE_TOKEN_EXPIRE_OFFSET', 60 * 5),
