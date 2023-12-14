f994469db21c60d1fdf2ce6c2c000aaa65f8cb6c
Aleksei Loginov <lex.loginov@gmail.com>
2023-12-14 18:19:30 +0100

    Revert "Revert "Add horizontal scrolling and implementation filter to vouchers tables""

--- app/Models/Voucher.php
+++ app/Models/Voucher.php
@@ -824,6 +824,12 @@ class Voucher extends BaseModel
             });
         }
 
+        if ($request->has('implementation_id') && $request->input('implementation_id')) {
+            $query->whereRelation('fund.fund_config', [
+                'implementation_id' => $request->input('implementation_id'),
+            ]);
+        }
+
         if ($expired !== null) {
             $query = $expired ? VoucherQuery::whereExpired($query) : VoucherQuery::whereNotExpired($query);
         }
