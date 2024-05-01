feb9b39a716f93406fcea519db78cba488661bdc
dev-rminds <dev@rminds.nl>
2024-05-01 13:25:24 +0300

    update fund-request validation policy texts

--- resources/lang/nl/policies/fund_requests.php
+++ resources/lang/nl/policies/fund_requests.php
@@ -1,5 +1,7 @@
 <?php
 
 return [
-      'fund_request_replaced' => 'Er is al een open aanvraag voor dit persoon. Behandel deze om verder te gaan.',
+    'fund_request_replaced' => 'Er is al een open aanvraag voor dit persoon. Behandel deze om verder te gaan.',
+    'fund_not_active' => 'Fund not active.',
+    'approved_request_exists' => 'Approved request already exists.',
 ];
