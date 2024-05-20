60c3d7a417242047458e4f89b95e3cbfc0e6ae0a
dev-rminds <dev@rminds.nl>
2024-05-20 15:50:59 +0300

    fund formula resource minor refactoring and record type preload

--- app/Http/Resources/FundFormulaResource.php
+++ app/Http/Resources/FundFormulaResource.php
@@ -3,12 +3,11 @@
 namespace App\Http\Resources;
 
 use App\Models\FundFormula;
-use Illuminate\Http\Resources\Json\JsonResource;
 
 /**
  * @property FundFormula $resource
  */
-class FundFormulaResource extends JsonResource
+class FundFormulaResource extends BaseJsonResource
 {
     /**
      * Transform the resource into an array.
@@ -19,10 +18,11 @@ class FundFormulaResource extends JsonResource
     public function toArray($request): array
     {
         return array_merge($this->resource->only([
-            'id', 'type', 'amount', 'amount_locale', 'record_type_key', 'updated_at',
+            'id', 'type', 'amount', 'amount_locale', 'record_type_key',
         ]), [
             'record_type_name' => $this->resource->record_type?->name,
-            'updated_at_locale' => format_datetime_locale($this->resource->updated_at),
-        ]);
+        ], $this->makeTimestamps($this->resource->only([
+            'created_at', 'updated_at',
+        ])));
     }
 }
