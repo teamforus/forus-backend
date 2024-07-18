c6be8d6955c1ea85e38ae88febb737fca3698d1e
Yaroslav Kosterin <ateamcms@gmail.com>
2024-07-18 22:22:44 +0300

    rename param token to exchangeToken in backend view

--- resources/views/pages/auth/deep_link.blade.php
+++ resources/views/pages/auth/deep_link.blade.php
@@ -6,7 +6,7 @@
         <div id="root"></div>
 
         <div id="params"
-             data-token="{{ $exchangeToken ?? '' }}"
+             data-exchange-token="{{ $exchangeToken ?? '' }}"
              data-type="{{ $type }}"
              data-mobile="{{ request()->userAgent() && user_agent_data(request()->userAgent())->isMobile() ? 'true' : 'false' }}"
              data-api-url="{{ url('api/v1') }}"
