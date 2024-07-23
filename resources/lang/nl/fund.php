72ac06533d7499cddbc2e59c6e1eaba78d5b17d1
Aleksei Loginov <lex.loginov@gmail.com>
2024-07-23 17:01:42 +0200

    translations

--- resources/lang/nl/fund.php
+++ resources/lang/nl/fund.php
@@ -5,11 +5,11 @@ return [
     'state_pending' => 'Dit fonds is niet actief',
     'type_external' => 'Dit fonds is niet actief',
     'taken_by_partner' => 'Uw partner heeft dit tegoed al aangevraagd!',
-    'backoffice_error' => 'The sponsor api of the fund is not responding, please try again later.',
+    'backoffice_error' => 'De backoffice-API van de sponsor reageert momenteel niet. Probeer het later opnieuw.',
     'already_received' => 'U heeft dit tegoed al ontvangen. Bekijk uw tegoeden bij "Mijn tegoeden".',
     'unmet_criteria' => 'U voldoet nog niet aan alle voorwaarden voor dit fonds.',
     'default_contact_info_message' => implode(' ', [
-        'Please provide contact information we can use to contact you',
-        'in case we need to clarify any details during application review.',
+        'Wij verzoeken je om je contactgegevens te vermelden, zodat we je eenvoudig kunnen bereiken.',
+        'Indien we tijdens de beoordeling aanvullende informatie nodig hebben, zullen we via de opgegeven contactgegevens contact met je opnemen.',
     ]),
 ];
