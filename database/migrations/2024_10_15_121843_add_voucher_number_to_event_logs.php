83419b1ec33f198989117fef8f520c217adbbbc1
dev-rminds <dev@rminds.nl>
2024-10-15 13:43:04 +0300

    add voucher number to event_logs

---
+++ database/migrations/2024_10_15_121843_add_voucher_number_to_event_logs.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('event_logs', function (Blueprint $table) {
+            $vouchers = DB::table('vouchers')->get();
+
+            foreach ($vouchers as $voucher) {
+                DB::table('event_logs')
+                    ->where('loggable_type', 'voucher')
+                    ->where('loggable_id', $voucher->id)
+                    ->update([
+                        'data->voucher_number' => $voucher->number,
+                    ]);
+            }
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void {}
+};
