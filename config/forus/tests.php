6552101e0e3c271cd1723fbd432257917c2efcfb
dev-rminds <dev@rminds.nl>
2024-10-28 10:46:34 +0200

    increase default dusk wait for time

--- config/forus/tests.php
+++ config/forus/tests.php
@@ -5,5 +5,5 @@ return [
     'identity_domain' => env('TESTS_IDENTITY_DOMAIN', 'example.com'),
 
     'dusk_selector' => env('DUSK_SELECTOR', 'data-dusk'),
-    'dusk_wait_for_time' => env('DUSK_WAIT_FOR_TIME', 15),
+    'dusk_wait_for_time' => env('DUSK_WAIT_FOR_TIME', 20),
 ];
