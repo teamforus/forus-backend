86e7ff4637465e00c1f9800ddc54c4ab6986f2f0
dev-rminds <dev@rminds.nl>
2024-11-08 12:08:45 +0200

    add offset configuration for reservations and declarations from expired funds

---
+++ database/migrations/2024_11_06_072241_add_reservation_and_reimbursement_days_offset_to_fund_configs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('fund_configs', function (Blueprint $table) {
+            $table->integer('reservation_approve_offset')
+                ->default(7)
+                ->after('csv_primary_key');
+
+            $table->integer('reimbursement_approve_offset')
+                ->default(7)
+                ->after('reservation_approve_offset');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('fund_configs', function (Blueprint $table) {
+            $table->dropColumn('reservation_approve_offset');
+            $table->dropColumn('reimbursement_approve_offset');
+        });
+    }
+};
