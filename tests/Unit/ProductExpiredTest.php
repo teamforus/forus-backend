40ab031fa6151d13b4cf26c319350b28b9a4445d
Yaroslav Kosterin <ateamcms@gmail.com>
2025-02-05 13:10:42 +0200

    add unit test for product expired

---
+++ tests/Unit/ProductExpiredTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Unit;
+
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tests\TestCase;
+use Tests\Traits\MakesTestFundProviders;
+
+class ProductExpiredTest extends TestCase
+{
+    use MakesTestFundProviders;
+    use DatabaseTransactions;
+
+    /**
+     * @return void
+     */
+    public function testProductExpired(): void
+    {
+        $product = $this->makeProductsFundFund(1)[0];
+        $this->assertFalse($product->expired);
+
+        $product->update(['expire_at' => now()]);
+        $this->assertFalse($product->expired, 'Product should not be expired if expire_at is today');
+
+        $this->travelTo(now()->addDay());
+        $this->assertTrue($product->refresh()->expired);
+    }
+}
