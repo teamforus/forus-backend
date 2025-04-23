577d66bcc8bfc0d8f1549712c457abdedbe625f3
dev-rminds <dev@rminds.nl>
2025-04-23 10:04:05 +0300

    make user profiles sortable

--- app/Searches/Sponsor/IdentitiesSearch.php
+++ app/Searches/Sponsor/IdentitiesSearch.php
@@ -3,13 +3,29 @@
 namespace App\Searches\Sponsor;
 
 use App\Models\Identity;
+use App\Models\IdentityEmail;
+use App\Models\ProfileRecord;
+use App\Models\Record;
 use App\Scopes\Builders\IdentityQuery;
 use App\Searches\BaseSearch;
+use App\Services\Forus\Session\Models\Session;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Carbon;
+use InvalidArgumentException;
 
 class IdentitiesSearch extends BaseSearch
 {
+    public const array SORT_BY_RECORD_TYPE = [
+        'given_name', 'family_name', 'client_number', 'birth_date',
+        'city', 'street', 'house_number', 'house_number_addition', 'postal_code', 'municipality_name',
+        'neighborhood_name',
+    ];
+
+    public const array SORT_BY = [
+        'id', 'email', 'bsn',  'last_activity', 'last_login', 'created_at',
+        ...self::SORT_BY_RECORD_TYPE,
+    ];
+
     /**
      * @return Builder|null
      */
@@ -18,16 +34,23 @@ class IdentitiesSearch extends BaseSearch
         /** @var Builder|Identity $builder */
         $builder = parent::query();
 
-        if ($this->getFilter('q')) {
-            $builder = $this->querySearchIdentity($builder, $this->getFilter('q'));
+        $orderBy = $this->getFilter('order_by', 'created_at');
+        $orderDir = $this->getFilter('order_dir', 'desc');
+        $fundId = $this->getFilter('fund_id');
+        $organizationId = $this->getFilter('organization_id');
+
+        if (empty($organizationId) || !is_numeric($organizationId)) {
+            throw new InvalidArgumentException("Invalid organizationId field: $organizationId");
         }
 
-        if ($this->getFilter('organization_id')) {
-            $builder = IdentityQuery::relatedToOrganization(
-                $builder,
-                $this->getFilter('organization_id'),
-                $this->getFilter('fund_id'),
-            );
+        if (!in_array($orderBy, self::SORT_BY)) {
+            throw new InvalidArgumentException("Invalid sort field: $orderBy");
+        }
+
+        $builder = IdentityQuery::relatedToOrganization($builder, $organizationId, $fundId);
+
+        if ($this->getFilter('q')) {
+            $builder = $this->querySearchIdentity($builder, $this->getFilter('q'), $organizationId);
         }
 
         if ($this->hasFilter('has_bsn')) {
@@ -42,38 +65,58 @@ class IdentitiesSearch extends BaseSearch
             }
         }
 
-        if ($birth_date_from = $this->getFilter('birth_date_from')) {
-            $builder->whereHas('profiles.profile_records', function (Builder $builder) use ($birth_date_from) {
-                $builder->whereHas('record_type', fn (Builder $q) => $q->where('key', 'birth_date'));
-                $builder->where('value', '>=', Carbon::parse($birth_date_from)->startOfDay());
+        if ($birthDateFrom = $this->getFilter('birth_date_to')) {
+            $builder->whereHas('profiles', function (Builder $builder) use ($birthDateFrom, $organizationId) {
+                $builder->where('organization_id', $organizationId);
+
+                $builder->whereHas('profile_records', function (Builder $builder) use ($birthDateFrom) {
+                    $builder->whereHas('record_type', fn (Builder $q) => $q->where('key', 'birth_date'));
+                    $builder->where('value', '>=', Carbon::parse($birthDateFrom)->startOfDay());
+                });
             });
         }
 
-        if ($birth_date_to = $this->getFilter('birth_date_to')) {
-            $builder->whereHas('profiles.profile_records', function (Builder $builder) use ($birth_date_to) {
-                $builder->whereHas('record_type', fn (Builder $q) => $q->where('key', 'birth_date'));
-                $builder->where('value', '<=', Carbon::parse($birth_date_to)->endOfDay());
+        if ($birthDateTo = $this->getFilter('birth_date_to')) {
+            $builder->whereHas('profiles', function (Builder $builder) use ($birthDateTo, $organizationId) {
+                $builder->where('organization_id', $organizationId);
+
+                $builder->whereHas('profile_records', function (Builder $builder) use ($birthDateTo) {
+                    $builder->whereHas('record_type', fn (Builder $q) => $q->where('key', 'birth_date'));
+                    $builder->where('value', '<=', Carbon::parse($birthDateTo)->endOfDay());
+                });
             });
         }
 
-        if ($postal_code = $this->getFilter('postal_code')) {
-            $builder->whereHas('profiles.profile_records', function (Builder $builder) use ($postal_code) {
-                $builder->whereHas('record_type', fn (Builder $q) => $q->where('key', 'postal_code'));
-                $builder->where('value', 'like', "%$postal_code%");
+        if ($postalCode = $this->getFilter('postal_code')) {
+            $builder->whereHas('profiles', function (Builder $builder) use ($postalCode, $organizationId) {
+                $builder->where('organization_id', $organizationId);
+
+                $builder->whereHas('profile_records', function (Builder $builder) use ($postalCode) {
+                    $builder->whereHas('record_type', fn (Builder $q) => $q->where('key', 'postal_code'));
+                    $builder->where('value', 'like', "%$postalCode%");
+                });
             });
         }
 
         if ($city = $this->getFilter('city')) {
-            $builder->whereHas('profiles.profile_records', function (Builder $builder) use ($city) {
-                $builder->whereHas('record_type', fn (Builder $q) => $q->where('key', 'city'));
-                $builder->where('value', 'like', "%$city%");
+            $builder->whereHas('profiles', function (Builder $builder) use ($city, $organizationId) {
+                $builder->where('organization_id', $organizationId);
+
+                $builder->whereHas('profile_records', function (Builder $builder) use ($city) {
+                    $builder->whereHas('record_type', fn (Builder $q) => $q->where('key', 'city'));
+                    $builder->where('value', 'like', "%$city%");
+                });
             });
         }
 
         if ($municipality = $this->getFilter('municipality_name')) {
-            $builder->whereHas('profiles.profile_records', function (Builder $builder) use ($municipality) {
-                $builder->whereHas('record_type', fn (Builder $q) => $q->where('key', 'municipality_name'));
-                $builder->where('value', 'like', "%$municipality%");
+            $builder->whereHas('profiles', function (Builder $builder) use ($municipality, $organizationId) {
+                $builder->where('organization_id', $organizationId);
+
+                $builder->whereHas('profile_records', function (Builder $builder) use ($municipality) {
+                    $builder->whereHas('record_type', fn (Builder $q) => $q->where('key', 'municipality_name'));
+                    $builder->where('value', 'like', "%$municipality%");
+                });
             });
         }
 
@@ -101,26 +144,100 @@ class IdentitiesSearch extends BaseSearch
             });
         }
 
-        return $builder->latest()->latest('id');
+        return $this->order($builder, $organizationId, $orderBy, $orderDir);
+    }
+
+    public function order(
+        Builder $builder,
+        int $organizationId,
+        string $orderBy = 'created_at',
+        string $orderDir = 'desc',
+    ): Builder {
+        if (in_array($orderBy, self::SORT_BY_RECORD_TYPE)) {
+            $builder->addSelect([
+                '__sort' => ProfileRecord::query()
+                    ->whereHas('profile', function (Builder $builder) use ($organizationId) {
+                        $builder->whereColumn('identity_id', 'identities.id');
+                        $builder->where('organization_id', $organizationId);
+                    })
+                    ->whereRelation('record_type', 'key', $orderBy)
+                    ->latest('created_at')
+                    ->select('value'),
+            ]);
+
+            return Identity::query()->fromSub($builder, 'list')->orderBy('__sort', $orderDir);
+        }
+
+        if ($orderBy == 'last_activity') {
+            $builder->addSelect([
+                '__sort' => Session::query()
+                    ->whereColumn('identity_address', 'address')
+                    ->latest('last_activity_at')
+                    ->select('last_activity_at'),
+            ]);
+
+            return Identity::query()->fromSub($builder, 'list')->orderBy('__sort', $orderDir);
+        }
+
+        if ($orderBy == 'last_login') {
+            $builder->addSelect([
+                '__sort' => Session::query()
+                    ->whereColumn('identity_address', 'address')
+                    ->latest('created_at')
+                    ->select('created_at'),
+            ]);
+
+            return Identity::query()->fromSub($builder, 'list')->orderBy('__sort', $orderDir);
+        }
+
+        if ($orderBy == 'email') {
+            $builder->addSelect([
+                '__sort' => IdentityEmail::query()
+                    ->whereColumn('identity_address', 'address')
+                    ->where('primary', true)
+                    ->latest()->select('email'),
+            ]);
+
+            return Identity::query()->fromSub($builder, 'list')->orderBy('__sort', $orderDir);
+        }
+
+        if ($orderBy == 'bsn') {
+            $builder->addSelect([
+                '__sort' => Record::where(function (Builder $builder) use ($orderBy) {
+                    $builder->whereColumn('identity_address', 'address');
+                    $builder->whereRelation('record_type', 'key', 'bsn');
+                })->latest('created_at')->select('value'),
+            ]);
+
+            return Identity::query()->fromSub($builder, 'list')->orderBy('__sort', $orderDir);
+        }
+
+        return $builder->orderBy($orderBy, $orderDir);
     }
 
     /**
      * @param Builder|Identity $builder
      * @param string $q
+     * @param int $organizationId
      * @return Builder|Identity
      */
-    public function querySearchIdentity(Builder|Identity $builder, string $q): Builder|Identity
+    public function querySearchIdentity(Builder|Identity $builder, string $q, int $organizationId): Builder|Identity
     {
-        return $builder->where(function (Builder $builder) use ($q) {
+        return $builder->where(function (Builder $builder) use ($q, $organizationId) {
             $builder->whereRelation('primary_email', 'email', 'like', "%$q%");
             $builder->orWhereRelation('emails_verified', 'email', 'like', "%$q%");
             $builder->orWhereRelation('record_bsn', 'value', 'like', "%$q%");
-            $builder->orWhereHas('profiles.profile_records', function (Builder $builder) use ($q) {
-                $builder->where('value', 'like', "%$q%");
-                $builder->whereHas('record_type', fn (Builder $q) => $q->whereIn('key', [
-                    'given_name', 'family_name', 'mobile', 'city', 'street', 'house_number', 'postal_code',
-                    'client_number', 'municipality_name', 'neighborhood_name',
-                ]));
+
+            $builder->orWhereHas('profiles', function (Builder $builder) use ($q, $organizationId) {
+                $builder->where('organization_id', $organizationId);
+
+                $builder->whereHas('profile_records', function (Builder $builder) use ($q) {
+                    $builder->where('value', 'like', "%$q%");
+                    $builder->whereHas('record_type', fn (Builder $q) => $q->whereIn('key', [
+                        'given_name', 'family_name', 'mobile', 'city', 'street', 'house_number', 'postal_code',
+                        'client_number', 'municipality_name', 'neighborhood_name',
+                    ]));
+                });
             });
         });
     }
