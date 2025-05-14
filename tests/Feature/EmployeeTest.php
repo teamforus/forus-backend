3832962b234a6135976a31686c4f946ff4ce1f3c
Yaroslav Kosterin <ateamcms@gmail.com>
2025-05-14 11:30:14 +0300

    add employee autotest

--- tests/Feature/EmployeeTest.php
+++ tests/Feature/EmployeeTest.php
@@ -4,27 +4,26 @@ namespace Tests\Feature;
 
 use App\Models\Employee;
 use App\Models\Organization;
+use App\Models\Permission;
 use App\Models\Role;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Collection;
 use Tests\TestCase;
 use Tests\Traits\MakesTestIdentities;
+use Tests\Traits\MakesTestOrganizations;
 
 class EmployeeTest extends TestCase
 {
-    use DatabaseTransactions;
     use WithFaker;
     use MakesTestIdentities;
-
-    /**
-     * @var string
-     */
-    protected string $apiMediaUrl = '/api/v1/platform/organizations/%s/employees';
+    use DatabaseTransactions;
+    use MakesTestOrganizations;
 
     /**
      * @var array|string[]
      */
-    protected array $resourceStructure = [
+    protected array $employeeResourceStructure = [
         'id',
         'email',
         'identity_address',
@@ -39,21 +38,23 @@ class EmployeeTest extends TestCase
      */
     public function testEmployeeStore(): void
     {
-        $organization = Organization::first();
-        $this->assertNotNull($organization);
-        $headers = $this->makeApiHeaders($organization->identity);
-        $employeeData = $this->makeEmployeeData();
+        $identity = $this->makeIdentity();
+        $organization = $this->makeTestOrganization($identity);
+        $headers = $this->makeApiHeaders($identity);
+        $employeeData = $this->makeEmployeeData(['admin', 'finance']);
 
         // make valid store employee request
-        $this->storeEmployee($organization, $employeeData, $headers);
+        $employee = $this->storeEmployee($organization, $employeeData, $headers);
+        $this->checkEmployeePermissions($organization, $employee, Role::byKey('admin')->permissions);
+        $this->checkEmployeePermissions($organization, $employee, Role::byKey('finance')->permissions);
 
         // test missing roles
-        $this->post($this->getListUrl($organization), [
+        $this->postJson("/api/v1/platform/organizations/$organization->id/employees", [
             'email' => $employeeData['email'],
         ], $headers)->assertJsonValidationErrorFor('roles');
 
         // test missing email
-        $this->post(sprintf($this->apiMediaUrl, $organization->id), [
+        $this->postJson("/api/v1/platform/organizations/$organization->id/employees", [
             'roles' => $employeeData['roles'],
         ], $headers)->assertJsonValidationErrorFor('email');
     }
@@ -63,21 +64,44 @@ class EmployeeTest extends TestCase
      */
     public function testEmployeeUpdate(): void
     {
-        $organization = Organization::first();
-        $this->assertNotNull($organization);
-        $headers = $this->makeApiHeaders($organization->identity);
+        $identity = $this->makeIdentity();
+        $organization = $this->makeTestOrganization($identity);
+        $headers = $this->makeApiHeaders($identity);
 
-        $employeeData = $this->makeEmployeeData();
+        $employeeData = $this->makeEmployeeData(['admin', 'finance']);
         $employee = $this->storeEmployee($organization, $employeeData, $headers);
 
-        /** @var Role $role */
-        $roles = $this->getRandomRoleIds(2, $employeeData['roles']);
+        $roles = Role::whereIn('key', ['validation', 'supervisor_validator'])->pluck('id')->toArray();
         $employeeData = array_merge($employeeData, compact('roles'));
-        $response = $this->patch($this->getItemUrl($organization, $employee), $employeeData, $headers);
-        $response->assertJsonStructure(['data' => $this->resourceStructure]);
+
+        $response = $this->patchJson(
+            "/api/v1/platform/organizations/$organization->id/employees/$employee->id",
+            $employeeData,
+            $headers
+        );
+
+        $response->assertJsonStructure(['data' => $this->employeeResourceStructure]);
 
         $employee->unsetRelation('roles');
         $this->assertTrue($employee->roles->pluck('id')->diff($employeeData['roles'])->isEmpty());
+
+        $newPermissions = Role::whereIn('key', ['validation', 'supervisor_validator'])
+            ->get()
+            ->pluck('permissions')
+            ->collapse()
+            ->unique('id');
+
+        $this->checkEmployeePermissions($organization, $employee, $newPermissions);
+
+        $newPermissionsIds = $newPermissions->pluck('id')->toArray();
+        $prevPermissions = Role::whereIn('key', ['admin', 'finance'])
+            ->get()
+            ->pluck('permissions')
+            ->collapse()
+            ->unique('id')
+            ->filter(fn (Permission $p) => !in_array($p->id, $newPermissionsIds));
+
+        $this->checkEmployeePermissions($organization, $employee, $prevPermissions, false);
     }
 
     /**
@@ -85,15 +109,30 @@ class EmployeeTest extends TestCase
      */
     public function testEmployeeDelete(): void
     {
-        $organization = Organization::where('name', 'Nijmegen')->first();
-        $this->assertNotNull($organization);
-        $headers = $this->makeApiHeaders($organization->identity);
+        $identity = $this->makeIdentity();
+        $organization = $this->makeTestOrganization($identity);
+        $owner = $organization->findEmployee($identity);
 
-        $employeeData = $this->makeEmployeeData();
-        $employee = $this->storeEmployee($organization, $employeeData, $headers);
+        $employeeData = $this->makeEmployeeData(['admin', 'finance']);
+        $employee = $this->storeEmployee($organization, $employeeData, $this->makeApiHeaders($identity));
 
-        $response = $this->deleteJson($this->getItemUrl($organization, $employee), [], $headers);
-        $response->assertSuccessful();
+        $this->deleteJson(
+            "/api/v1/platform/organizations/$organization->id/employees/$employee->id",
+            [],
+            $this->makeApiHeaders($employee->identity)
+        )->assertForbidden();
+
+        $this->deleteJson(
+            "/api/v1/platform/organizations/$organization->id/employees/$owner->id",
+            [],
+            $this->makeApiHeaders($employee->identity)
+        )->assertForbidden();
+
+        $this->deleteJson(
+            "/api/v1/platform/organizations/$organization->id/employees/$employee->id",
+            [],
+            $this->makeApiHeaders($identity)
+        )->assertSuccessful();
 
         $this->assertNull(Employee::find($employee->id));
     }
@@ -103,11 +142,18 @@ class EmployeeTest extends TestCase
      */
     public function testEmployeeShow(): void
     {
-        $organization = Organization::where('name', 'Nijmegen')->first();
-        $this->assertNotNull($organization);
-        $headers = $this->makeApiHeaders($organization->identity);
+        $identity = $this->makeIdentity();
+        $organization = $this->makeTestOrganization($identity);
+        $headers = $this->makeApiHeaders($identity);
 
-        $this->storeEmployee($organization, $this->makeEmployeeData(), $headers);
+        $employee = $this->storeEmployee($organization, $this->makeEmployeeData(['admin', 'finance']), $headers);
+
+        $response = $this->getJson(
+            "/api/v1/platform/organizations/$organization->id/employees/$employee->id",
+            $headers
+        );
+
+        $response->assertJsonStructure(['data' => $this->employeeResourceStructure]);
     }
 
     /**
@@ -115,11 +161,11 @@ class EmployeeTest extends TestCase
      */
     public function testEmployeeSearch(): void
     {
-        $organization = Organization::where('name', 'Nijmegen')->first();
-        $this->assertNotNull($organization);
-        $headers = $this->makeApiHeaders($organization->identity);
+        $identity = $this->makeIdentity();
+        $organization = $this->makeTestOrganization($identity);
+        $headers = $this->makeApiHeaders($identity);
 
-        $employeeData = $this->makeEmployeeData();
+        $employeeData = $this->makeEmployeeData(['admin', 'finance']);
         $employee = $this->storeEmployee($organization, $employeeData, $headers);
 
         // make search by email query
@@ -127,12 +173,12 @@ class EmployeeTest extends TestCase
         $params = http_build_query(compact('q'));
 
         // make search request
-        $response = $this->getJson($this->getListUrl($organization) . "?$params", $headers);
+        $response = $this->getJson("/api/v1/platform/organizations/$organization->id/employees?$params", $headers);
         $response->assertSuccessful();
         $this->assertIsArray($response->json('data'));
 
         // only one employee should be returned and it should be the requested employee
-        $this->assertTrue(count($response->json('data')) == 1);
+        $this->assertCount(1, $response->json('data'));
         $this->assertTrue($response->json('data.0.id') == $employee->id);
 
         // search by permission
@@ -140,15 +186,74 @@ class EmployeeTest extends TestCase
         $params = http_build_query(compact('permission'));
 
         // make search by permission query
-        $response = $this->getJson($this->getListUrl($organization) . "?$params", $headers);
+        $response = $this->getJson("/api/v1/platform/organizations/$organization->id/employees?$params", $headers);
         $response->assertSuccessful();
         $this->assertIsArray($response->json('data'));
+        $data = collect($response->json('data'));
 
-        $employee = Employee::find($response->json('data.0.id'));
-        $this->assertNotNull($employee);
-        $this->assertTrue($organization->employeesWithPermissions($permission)
-            ->where('id', $employee->id)
-            ->isNotEmpty());
+        // only two employees should be returned (base when created organization and the second which was created)
+        $this->assertCount(2, $data);
+
+        $validEmployeeIds = [
+            $organization->findEmployee($identity)->id,
+            $employee->id,
+        ];
+
+        $this->assertTrue($data->pluck('id')->diff($validEmployeeIds)->isEmpty());
+
+        $data->each(function ($item) use ($organization, $permission) {
+            $employee = Employee::find($item['id']);
+            $this->assertNotNull($employee);
+
+            $this->assertTrue(
+                $organization->employeesWithPermissions($permission)
+                    ->where('id', $employee->id)
+                    ->isNotEmpty()
+            );
+        });
+    }
+
+    /**
+     * @return void
+     */
+    public function testEmployeeTransferOwnership(): void
+    {
+        $identity = $this->makeIdentity();
+        $organization = $this->makeTestOrganization($identity);
+        $headers = $this->makeApiHeaders($identity);
+
+        $this->assertTrue($organization->isOwner($identity));
+
+        // make employee with a finance role
+        $employeeData = $this->makeEmployeeData(['finance']);
+        $employee = $this->storeEmployee($organization, $employeeData, $headers);
+
+        // assert validation error when employee doesnt have admin role
+        $this->patchJson(
+            "/api/v1/platform/organizations/$organization->id/transfer-ownership",
+            ['employee_id' => $employee->id],
+            $headers
+        )->assertJsonValidationErrors(['employee_id']);
+
+        // add an admin role to employee and assert transfer success
+        $roles = Role::whereIn('key', ['admin'])->pluck('id')->toArray();
+        $employeeData = array_merge($employeeData, compact('roles'));
+
+        $this->patchJson(
+            "/api/v1/platform/organizations/$organization->id/employees/$employee->id",
+            $employeeData,
+            $headers
+        )->assertSuccessful();
+
+        $this->patchJson(
+            "/api/v1/platform/organizations/$organization->id/transfer-ownership",
+            ['employee_id' => $employee->id],
+            $headers
+        )->assertSuccessful();
+
+        $organization->refresh();
+        $this->assertFalse($organization->isOwner($identity));
+        $this->assertTrue($organization->isOwner($employee->identity));
     }
 
     /**
@@ -162,8 +267,13 @@ class EmployeeTest extends TestCase
         array $employeeData,
         array $headers = []
     ): Employee {
-        $response = $this->post($this->getListUrl($organization), $employeeData, $headers);
-        $response->assertJsonStructure(['data' => $this->resourceStructure]);
+        $response = $this->postJson(
+            "/api/v1/platform/organizations/$organization->id/employees",
+            $employeeData,
+            $headers
+        );
+
+        $response->assertJsonStructure(['data' => $this->employeeResourceStructure]);
 
         $employee = Employee::find($response->json('data.id'));
         $this->assertNotNull($employee);
@@ -174,46 +284,34 @@ class EmployeeTest extends TestCase
     }
 
     /**
-     * @param int $count
-     * @param array $notIn
+     * @param array $roles
      * @return array
      */
-    protected function getRandomRoleIds(int $count = 1, array $notIn = []): array
-    {
-        return Role::inRandomOrder()
-            ->whereNotIn('id', $notIn)
-            ->limit($count)
-            ->pluck('id')
-            ->toArray();
-    }
-
-    /**
-     * @return array
-     */
-    protected function makeEmployeeData(): array
+    protected function makeEmployeeData(array $roles): array
     {
         return [
-            'roles' => $this->getRandomRoleIds(2),
+            'roles' => Role::whereIn('key', $roles)->pluck('id')->toArray(),
             'email' => $this->makeUniqueEmail(),
         ];
     }
 
     /**
      * @param Organization $organization
-     * @return string
-     */
-    protected function getListUrl(Organization $organization): string
-    {
-        return sprintf($this->apiMediaUrl, $organization->id);
-    }
-
-    /**
-     * @param Organization $organization
      * @param Employee $employee
-     * @return string
+     * @param Collection $permissions
+     * @param bool $assert
+     * @return void
      */
-    protected function getItemUrl(Organization $organization, Employee $employee): string
-    {
-        return sprintf($this->apiMediaUrl, $organization->id) . '/' . $employee->id;
+    private function checkEmployeePermissions(
+        Organization $organization,
+        Employee $employee,
+        Collection $permissions,
+        bool $assert = true,
+    ): void {
+        foreach ($permissions as $permission) {
+            $assert
+                ? $this->assertTrue($organization->identityCan($employee->identity, $permission->key))
+                : $this->assertFalse($organization->identityCan($employee->identity, $permission->key));
+        }
     }
 }
