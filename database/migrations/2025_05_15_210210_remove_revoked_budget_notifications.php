85d74d67ae4c5d7303d71280a03a5ed825322feb
Yaroslav Kosterin <ateamcms@gmail.com>
2025-05-16 19:21:16 +0300

    remove revoked_budget notification

---
+++ database/migrations/2025_05_15_210210_remove_revoked_budget_notifications.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        DB::table('email_logs')
+            ->where('system_notification_key', 'notifications_fund_providers.revoked_budget')
+            ->delete();
+
+        DB::table('notification_templates')
+            ->whereIn(
+                'system_notification_id',
+                DB::table('system_notifications')
+                    ->select('id')
+                    ->where('key', 'notifications_fund_providers.revoked_budget')
+            )
+            ->where('type', 'mail')
+            ->delete();
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
