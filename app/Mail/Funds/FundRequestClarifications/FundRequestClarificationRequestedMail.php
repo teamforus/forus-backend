9bf48e7db27cb68d177194b7c85ab7987bcc21ca
dev-rminds <dev@rminds.nl>
2025-08-12 15:43:21 +0300

    update clarification email texts and add icon

--- app/Mail/Funds/FundRequestClarifications/FundRequestClarificationRequestedMail.php
+++ app/Mail/Funds/FundRequestClarifications/FundRequestClarificationRequestedMail.php
@@ -5,6 +5,7 @@ namespace App\Mail\Funds\FundRequestClarifications;
 use App\Mail\ImplementationMail;
 use Illuminate\Mail\Mailable;
 use League\CommonMark\Exception\CommonMarkException;
+use Throwable;
 
 /**
  * Notify requester about fund request clarification being requested by the sponsor/validator.
@@ -32,7 +33,13 @@ class FundRequestClarificationRequestedMail extends ImplementationMail
         $question = $data['fund_request_clarification_question'] ?? '';
         $link = $data['webshop_clarification_link'];
 
+        try {
+            $icon = $this->headerIconImage(asset('/assets/img/mail-fund-request-feedback-logo.png'), 100);
+        } catch (Throwable) {
+        }
+
         return [
+            'icon' => $icon ?? null,
             'fund_request_clarification_question' => nl2br(e($question)),
             'webshop_clarification_link' => $this->makeLink($link, $linkTitle),
             'webshop_clarification_button' => $this->makeButton($link, $linkTitle),
