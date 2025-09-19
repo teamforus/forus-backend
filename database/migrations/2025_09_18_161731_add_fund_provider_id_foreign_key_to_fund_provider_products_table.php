5ba1b4a6c1a47d95d1bceac559dc52468344e876
Yaroslav Kosterin <ateamcms@gmail.com>
2025-09-19 12:26:50 +0300

    add foreign key to fund provider products table

---
+++ database/migrations/2025_09_18_161731_add_fund_provider_id_foreign_key_to_fund_provider_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('fund_provider_products', function (Blueprint $table) {
+            $table->foreign('fund_provider_id')
+                ->references('id')
+                ->on('fund_providers')
+                ->onDelete('restrict');
+        });
+
+        Schema::table('voucher_transaction_bulks', function (Blueprint $table) {
+            $table->enum('state', ['draft', 'error', 'pending', 'accepted', 'rejected'])->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('fund_provider_products', function (Blueprint $table) {
+            $table->dropForeign(['fund_provider_id']);
+        });
+
+        Schema::table('voucher_transaction_bulks', function (Blueprint $table) {
+            $table->string('state')->change();
+        });
+    }
+};
