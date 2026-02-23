0f80b3e649dfc3f29d882b06e4c780b712118053
Yaroslav Kosterin <ateamcms@gmail.com>
2026-02-23 10:14:02 +0200

    replace brp record type partner gender female with partner and children gender female

--- config/forus/children_age_groups.php
+++ config/forus/children_age_groups.php
@@ -26,7 +26,7 @@ return [
         'from' => 18,
         'to' => 99,
     ], [
-        'record_type_key' => 'children_age_group_12_17_gender_female',
+        'record_type_key' => 'children_age_group_12_17_gender_female_partner_female',
         'from' => 12,
         'to' => 17,
         'gender' => 'vrouw',
@@ -57,7 +57,7 @@ return [
         'from' => 18,
         'to' => 99,
     ], [
-        'record_type_key' => 'children_age_group_12_17_gender_female',
+        'record_type_key' => 'children_age_group_12_17_gender_female_partner_female',
         'from' => 12,
         'to' => 17,
         'gender' => 'vrouw',
