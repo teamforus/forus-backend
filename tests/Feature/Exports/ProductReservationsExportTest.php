f6d7a44edf11f307e49ed360e55ca1aee9d8d09a
dev-rminds <dev@rminds.nl>
2026-04-28 12:13:08 +0300

    add extra_amount export permission test case

--- tests/Feature/Exports/ProductReservationsExportTest.php
+++ tests/Feature/Exports/ProductReservationsExportTest.php
@@ -31,6 +31,11 @@ class ProductReservationsExportTest extends TestCase
     protected string $apiExportUrl = '/api/v1/platform/organizations/%s/product-reservations/export';
 
     /**
+     * @var string
+     */
+    protected string $apiExportFieldsUrl = '/api/v1/platform/organizations/%s/product-reservations/export-fields';
+
+    /**
      * @throws Throwable
      * @return void
      */
@@ -113,6 +118,59 @@ class ProductReservationsExportTest extends TestCase
         $this->assertExportedData($response, $reservation, [
             ProductReservationsExport::trans('code'),
         ]);
+    }
+
+    /**
+     * @throws Throwable
+     * @return void
+     */
+    public function testProductReservationsExportHandlesExtraAmountPermission(): void
+    {
+        $identity = $this->makeIdentity($this->makeUniqueEmail());
+        $sponsorOrganization = $this->makeTestOrganization($identity);
+        $fund = $this->makeTestFund($sponsorOrganization);
+
+        $voucher = $this->makeTestVoucher($fund, identity: $this->makeIdentity());
+        $provider = $this->makeTestProviderOrganization($this->makeIdentity());
+        $product = $this->createProductForReservation($provider, [$fund]);
+        $reservation = $this->makeReservation($voucher, $product);
+
+        $reservation->update(['amount_extra' => 7.5]);
+
+        // Assert extra amount field hidden without permission
+        $apiHeaders = $this->makeApiHeaders($this->makeIdentityProxy($provider->identity));
+        $response = $this->getJson(sprintf($this->apiExportFieldsUrl, $provider->id), $apiHeaders);
+
+        $response->assertStatus(200);
+        $this->assertNotContains('amount_extra', Arr::pluck($response->json('data'), 'key'));
+
+        // Assert extra amount field rejected without permission
+        $url = sprintf($this->apiExportUrl, $provider->id) . '?' . http_build_query([
+            'data_format' => 'csv',
+            'fields' => ['amount_extra'],
+        ]);
+
+        $this->getJson($url, $apiHeaders)->assertJsonValidationErrors('fields.0');
+
+        // Enable extra amount permission
+        $product->fund_providers()->where('fund_id', $fund->id)->first()->update(['allow_extra_payments' => true]);
+
+        // Assert extra amount field available with permission
+        $response = $this->getJson(sprintf($this->apiExportFieldsUrl, $provider->id), $apiHeaders);
+
+        $response->assertStatus(200);
+        $this->assertContains('amount_extra', Arr::pluck($response->json('data'), 'key'));
+
+        // Assert extra amount field exported with permission
+        $response = $this->getJson(sprintf($this->apiExportUrl, $provider->id) . '?' . http_build_query([
+            'data_format' => 'csv',
+            'fields' => ['amount_extra'],
+        ]), $apiHeaders);
+
+        $rows = $this->assertCsvExportResponse($response);
+
+        $this->assertExportHeaders($rows, [ProductReservationsExport::trans('amount_extra')]);
+        $this->assertEquals((float) $reservation->amount_extra, (float) $rows[1][0]);
     }
 
     /**
