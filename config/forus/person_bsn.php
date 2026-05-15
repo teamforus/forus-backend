40ebce0cd3fccc795bf07f04f96ab280de11a0b5
Yaroslav Kosterin <ateamcms@gmail.com>
2026-05-15 11:48:33 +0300

    unset age with birth date for brp test

--- config/forus/person_bsn.php
+++ config/forus/person_bsn.php
@@ -179,10 +179,12 @@ $default = [
 
 $missed_records = $default;
 unset($missed_records['999993112']['geboorte']);
+unset($missed_records['999993112']['leeftijd']);
 unset($missed_records['999993112']['naam']['voornamen']);
 unset($missed_records['999994542']['geboorte']);
+unset($missed_records['999994542']['leeftijd']);
 unset($missed_records['123456782']['geboorte']);
-
+unset($missed_records['123456782']['leeftijd']);
 
 return [
     'default' => env('PERSON_BSN_API_SERVICE', 'iconnect'),
